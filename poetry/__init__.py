cf9922cc68f170d75c27ffada4e89b4cbf2b128a
Sébastien Eustace <sebastien@eustace.io>
2018-04-02 15:59:12 -0500

    Add _vendor directory

--- poetry/__init__.py
+++ poetry/__init__.py
@@ -1,1 +1,10 @@
-from .__version__ import __version__
+import os
+import sys
+
+_ROOT = os.path.dirname(os.path.realpath(__file__))
+_VENDOR = os.path.join(_ROOT, 'vendor')
+
+# Add vendored dependencies to path.
+sys.path.insert(0, _VENDOR)
+
+from .__version__ import __version__  # noqa
