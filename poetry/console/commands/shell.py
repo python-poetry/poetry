3f0fc1c66a0fcc80a582e8e9c4870032b9ab5185
Cauê Baasch de Souza <cauebs@pm.me>
2018-06-11 14:24:23 -0300

    Add provisional Windows support; handle unset SHELL variable

--- poetry/console/commands/shell.py
+++ poetry/console/commands/shell.py
@@ -15,12 +15,19 @@ If one doesn't exist yet, it will be created.
 
     def handle(self):
         if self.venv.is_venv():
+            if self.venv._windows:
+                self.venv.execute("cmd", "/k")
+
+            else:
+                shell = self.venv.get_shell()
+                if shell is None:
+                    self.error("SHELL environment variable must be set.")
+                    return
+                self.venv.execute(shell)
+
             self.line(
                 "Spawning shell within <info>{}</>".format(self.venv.venv)
             )
-
-            shell = self.venv.get_shell()
-            self.venv.execute(shell)
 
         else:
             self.line("Virtual environment already activated.")
