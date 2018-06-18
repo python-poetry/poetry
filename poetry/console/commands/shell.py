80d25e98d0900d046e05d0ba54f818998d231f66
Cauê Baasch de Souza <cauebs@pm.me>
2018-06-18 13:59:11 -0300

    Add comments to shell command code

--- poetry/console/commands/shell.py
+++ poetry/console/commands/shell.py
@@ -16,8 +16,11 @@ If one doesn't exist yet, it will be created.
 """
 
     def handle(self):
+        # This variable will be set if the current shell was spawned by poetry
         current_venv = environ.get("POETRY_ACTIVE")
 
+        # Alternatively, a virtual environment might have already been
+        # activated in some other way
         if not current_venv and not self.venv.is_venv():
             current_venv = environ.get("VIRTUAL_ENV")
 
@@ -29,6 +32,8 @@ If one doesn't exist yet, it will be created.
             return
 
         if self.venv._windows:
+            # Windows is probably more complicated than this
+            # and support to cmd alternatives should probably be added
             shell, *args = ("cmd", "/k")
         else:
             shell, *args = (environ.get("SHELL"),)
@@ -38,6 +43,7 @@ If one doesn't exist yet, it will be created.
 
         self.line("Spawning shell within <info>{}</>".format(self.venv.venv))
 
+        # Setting this to avoid spawning unnecessary nested shells
         environ["POETRY_ACTIVE"] = str(self.venv.venv)
         self.venv.execute(shell, *args)
         environ.pop("POETRY_ACTIVE")
