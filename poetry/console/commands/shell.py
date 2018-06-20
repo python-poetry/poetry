00a53945abf39677be4f588c10bcefc353ecdb8c
Cauê Baasch de Souza <cauebs@pm.me>
2018-06-19 22:14:31 -0300

    Use utils.shell to get path to running shell

--- poetry/console/commands/shell.py
+++ poetry/console/commands/shell.py
@@ -17,6 +17,8 @@ If one doesn't exist yet, it will be created.
 """
 
     def handle(self):
+        from poetry.utils.shell import Shell
+
         # Check if it's already activated or doesn't exist and won't be created
         if strtobool(environ.get("POETRY_ACTIVE", "0")) or not self.venv.is_venv():
             current_venv = environ.get("VIRTUAL_ENV")
@@ -32,21 +34,10 @@ If one doesn't exist yet, it will be created.
 
             return
 
-        if self.venv._windows:
-            # Windows is probably more complicated than this
-            # and support to cmd alternatives should probably be added
-            shell = "cmd"
-            args = ["/k"]
-        else:
-            shell = environ.get("SHELL")
-            args = []
-            if shell is None:
-                self.error("The SHELL environment variable must be set.")
-                return
-
         self.line("Spawning shell within <info>{}</>".format(self.venv.venv))
 
         # Setting this to avoid spawning unnecessary nested shells
         environ["POETRY_ACTIVE"] = "1"
-        self.venv.execute(shell, *args)
+        shell = Shell.get()
+        self.venv.execute(shell.path)
         environ.pop("POETRY_ACTIVE")
