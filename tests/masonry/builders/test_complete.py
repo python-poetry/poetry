ce8c843e3476bae0a2e00bbb42c462a84c372bfe
Sébastien Eustace <sebastien@eustace.io>
2018-06-25 15:06:45 -0500

    Skip C extension test for Python <=3.4

--- tests/masonry/builders/test_complete.py
+++ tests/masonry/builders/test_complete.py
@@ -33,6 +33,7 @@ def clear_samples_dist():
             shutil.rmtree(str(dist))
 
 
+@pytest.mark.skipif(sys.platform == "win32" and sys.version_info <= (3, 4))
 def test_wheel_c_extension():
     module_path = fixtures_dir / "extended"
     builder = CompleteBuilder(Poetry.create(module_path), NullVenv(True), NullIO())
