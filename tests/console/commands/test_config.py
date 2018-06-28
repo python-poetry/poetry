42fb45133d73a18b8c5e1ec6af5f649fcdfdc859
Sébastien Eustace <sebastien@eustace.io>
2018-06-28 18:18:26 -0500

    Fix tests

--- tests/console/commands/test_config.py
+++ tests/console/commands/test_config.py
@@ -16,15 +16,25 @@ def config():
         return Config(TomlFile(f.name))
 
 
+@pytest.fixture(autouse=True)
+def setup(config):
+    config.add_property("settings.virtualenvs.path", ".")
+
+    yield
+
+    config.remove_property("settings.virtualenvs.path")
+
+
 def test_list_displays_default_value_if_not_set(app, config):
     command = app.find("config")
-    command._config = config
+    command._config = Config(config.file)
     tester = CommandTester(command)
 
     tester.execute([("command", command.get_name()), ("--list", True)])
 
     expected = """settings.virtualenvs.create = true
 settings.virtualenvs.in-project = false
+settings.virtualenvs.path = "."
 repositories = {}
 """
 
@@ -33,7 +43,7 @@ repositories = {}
 
 def test_list_displays_set_get_setting(app, config):
     command = app.find("config")
-    command._config = config
+    command._config = Config(config.file)
     tester = CommandTester(command)
 
     tester.execute(
@@ -49,6 +59,7 @@ def test_list_displays_set_get_setting(app, config):
 
     expected = """settings.virtualenvs.create = false
 settings.virtualenvs.in-project = false
+settings.virtualenvs.path = "."
 repositories = {}
 """
 
