230f7da73e5784af0fe11ed68bb52053baf9fbd1
Sébastien Eustace <sebastien@eustace.io>
2018-10-06 13:16:03 -0500

    Fix formatting

--- poetry/masonry/utils/tags.py
+++ poetry/masonry/utils/tags.py
@@ -84,7 +84,7 @@ def get_abi_tag(venv):
         if get_flag(
             venv,
             "Py_UNICODE_SIZE",
-            lambda: sys.maxunicode == 0x10ffff,
+            lambda: sys.maxunicode == 0x10FFFF,
             expected=4,
             warn=(impl == "cp" and venv.version_info < (3, 3)),
         ) and venv.version_info < (3, 3):
