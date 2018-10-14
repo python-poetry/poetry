09196fe2ba8ae39fe94ccc67b92224c1d5c445a3
Sébastien Eustace <sebastien@eustace.io>
2018-10-13 19:44:23 -0500

    Update jsonschema dependency

--- poetry/json/__init__.py
+++ poetry/json/__init__.py
@@ -22,7 +22,7 @@ def validate_object(obj, schema_name):  # type: (dict, str) -> List[str]
     with open(schema) as f:
         schema = json.loads(f.read())
 
-    validator = jsonschema.Draft4Validator(schema)
+    validator = jsonschema.Draft7Validator(schema)
     validation_errors = sorted(validator.iter_errors(obj), key=lambda e: e.path)
 
     errors = []
