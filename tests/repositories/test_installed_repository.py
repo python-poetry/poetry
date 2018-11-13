5bb1f6b77d8d5824b026eca4934659de31f82499
Philip Jones <philip.jones@smarkets.com>
2018-11-13 11:21:35 +0000

    Bugfix cope with arbitrary equals
    When loading installed packages pip freeze can output lines with
    ``===``, arbitrary equals, qualifiers instead of ``==``. This allows
    poetry to parse either.
    
    This fixes #433, based on the suggestion present in the discussion.

--- tests/repositories/test_installed_repository.py
+++ tests/repositories/test_installed_repository.py
@@ -4,6 +4,7 @@ from poetry.utils.env import MockEnv as BaseMockEnv
 
 FREEZE_RESULTS = """cleo==0.6.8
 -e git+https://github.com/sdispater/pendulum.git@bb058f6b78b2d28ef5d9a5e759cfa179a1a713d6#egg=pendulum
+orator===0.9.8
 """
 
 
@@ -18,7 +19,7 @@ class MockEnv(BaseMockEnv):
 def test_load():
     repository = InstalledRepository.load(MockEnv())
 
-    assert len(repository.packages) == 2
+    assert len(repository.packages) == 3
 
     cleo = repository.packages[0]
     assert cleo.name == "cleo"
@@ -30,3 +31,7 @@ def test_load():
     assert pendulum.source_type == "git"
     assert pendulum.source_url == "https://github.com/sdispater/pendulum.git"
     assert pendulum.source_reference == "bb058f6b78b2d28ef5d9a5e759cfa179a1a713d6"
+
+    orator = repository.packages[2]
+    assert orator.name == "orator"
+    assert orator.version.text == "0.9.8"
