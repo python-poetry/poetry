4f215278fb9547ef82bbc99f7ee21829dda11577
Andreas Sodeur <38456167+asodeur@users.noreply.github.com>
2019-03-06 22:43:12 +0100

    [724] handle ast.Name values in extras_require (#883)
    * fixing #724
    
    * added test for issue 724

---
+++ tests/utils/fixtures/setups/extras_require_with_vars/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+tests_require = ["pytest"]
+
+setup(
+    name="extras_require_with_vars",
+    version="0.0.1",
+    description="test setup_reader.py",
+    install_requires=[],
+    extras_require={"test": tests_require},
+)
