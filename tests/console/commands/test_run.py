5eed55f2f0c2cbe248cfd42b9b1b5a41df891b47
Christopher Dignam <chris@dignam.xyz>
2019-03-31 19:35:44 -0400

    Add basic test
    There are probably cleaner ways to test the CLI, so let me know.

---
+++ tests/console/commands/test_run.py
@@ -0,0 +1,32 @@
+import pytest
+
+from poetry.console.commands import RunCommand
+from poetry.utils.env import Env
+
+
+def test_run_script_sets_argv(app):
+    """
+    If RunCommand calls a script defined in pyproject.toml, sys.argv[0] should
+    be set to the full path of the script.
+    """
+
+    def mock_foo(bin):
+        return "/full/path/to/" + bin
+
+    def mock_run(*args, **kwargs):
+        return dict(args=args, kwargs=kwargs)
+
+    command = app.find("run")
+    command._env = Env.get()
+    command._env._bin = mock_foo
+    command._env.run = mock_run
+    res = command.run_script("cli:cli", ["foogit", "status"])
+    expected = dict(
+        args=(
+            "python",
+            "-c",
+            "\"import sys; from importlib import import_module; sys.argv = ['/full/path/to/foogit', 'status']; import_module('cli').cli()\"",
+        ),
+        kwargs={"call": True, "shell": True},
+    )
+    assert res == expected
