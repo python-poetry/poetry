2ce6a6d263443f5b55e9056c542990aedabbcb51
Sébastien Eustace <sebastien@eustace.io>
2019-05-10 10:51:53 +0200

    Improve handling of sources in pools

--- tests/repositories/test_pool.py
+++ tests/repositories/test_pool.py
@@ -11,3 +11,18 @@ def test_pool_raises_package_not_found_when_no_package_is_found():
 
     with pytest.raises(PackageNotFound):
         pool.package("foo", "1.0.0")
+
+
+def test_pool():
+    pool = Pool()
+
+    assert 0 == len(pool.repositories)
+    assert pool.default is None
+
+
+def test_pool_with_initial_repositories():
+    repo = Repository()
+    pool = Pool([repo])
+
+    assert 1 == len(pool.repositories)
+    assert pool.default is None
