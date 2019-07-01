193fafe796b0e11d5a6c21e7cb986814fca95638
Jennifer Wilcox <JENNIFER.WILCOX@adtran.com>
2019-07-01 11:32:43 -0500

    Fix issue when empty marker appears in lock

--- tests/packages/test_package.py
+++ tests/packages/test_package.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from poetry.packages import Package
+from poetry.version.markers import EmptyMarker
 
 
 def test_package_authors():
@@ -14,3 +15,11 @@ def test_package_authors():
     package.authors.insert(0, "John Doe")
     assert package.author_name == "John Doe"
     assert package.author_email is None
+
+
+def test_package_empty_marker():
+    package = Package("foo", "0.1.0")
+    package.marker = EmptyMarker()
+
+    dep = package.to_dependency()
+    assert dep.python_versions == "*"
