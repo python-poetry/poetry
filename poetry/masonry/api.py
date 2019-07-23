b9695129d34c5e0b6c7347849dabc5c27776129e
Hartorn <hartorn.github@gmail.com>
2019-07-23 17:57:57 +0200

    Fix mkdir breaking due to #1123
    Mkdir is breaking when folder is already existing, this allows it not to fail if target exists

--- poetry/masonry/api.py
+++ poetry/masonry/api.py
@@ -37,7 +37,7 @@ def prepare_metadata_for_build_wheel(metadata_directory, config_settings=None):
     builder = WheelBuilder(poetry, SystemEnv(Path(sys.prefix)), NullIO())
 
     dist_info = Path(metadata_directory, builder.dist_info)
-    dist_info.mkdir()
+    dist_info.mkdir(parents=True, exist_ok=True)
 
     if "scripts" in poetry.local_config or "plugins" in poetry.local_config:
         with (dist_info / "entry_points.txt").open("w", encoding="utf-8") as f:
