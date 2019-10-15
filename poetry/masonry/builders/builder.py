ce576c7040fcdab4e27b464b85250b02370de77f
finswimmer77@gmail.com <GiWahnsinn07!>
2019-10-15 20:44:21 +0200

    only glob a second time if path is dir

--- poetry/masonry/builders/builder.py
+++ poetry/masonry/builders/builder.py
@@ -89,16 +89,19 @@ class Builder(object):
         for excluded_glob in itertools.chain.from_iterable(
             map(
                 lambda excluded_path: glob(
-                    Path(self._path, excluded_path).as_posix(), recursive=True
+                    str(Path(self._path, excluded_path)), recursive=True
                 ),
                 self._package.exclude,
             )
         ):
             if Path(excluded_glob).is_dir():
-                excluded_glob = Path(excluded_glob, "**/*")
-            for excluded in glob(str(excluded_glob), recursive=True):
+                for excluded in glob(str(Path(excluded_glob, "**/*")), recursive=True):
+                    explicitely_excluded.add(
+                        Path(excluded).relative_to(self._path).as_posix()
+                    )
+            else:
                 explicitely_excluded.add(
-                    Path(excluded).relative_to(self._path).as_posix()
+                    Path(excluded_glob).relative_to(self._path).as_posix()
                 )
 
         ignored = vcs_ignored_files | explicitely_excluded
