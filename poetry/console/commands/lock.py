555644bd08af1d3f13aa7ef6526eaf65a550311c
nate giraldi <ng269@cornell.edu>
2019-10-23 23:21:25 -0400

    Fix typo in LockCommand `help`

--- poetry/console/commands/lock.py
+++ poetry/console/commands/lock.py
@@ -7,7 +7,7 @@ class LockCommand(EnvCommand):
     description = "Locks the project dependencies."
 
     help = """The <info>lock</info> command reads the <comment>pyproject.toml</> file from
-the current directory, processes it, and locks the depdencies in the <comment>poetry.lock</> file.
+the current directory, processes it, and locks the dependencies in the <comment>poetry.lock</> file.
 
 <info>poetry lock</info>
 """
