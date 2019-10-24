49a7a86896447b1211ce83c265dc7210b3b83d24
Sébastien Eustace <sebastien@eustace.io>
2019-10-24 21:36:00 +0200

    Fix the new command

--- poetry/console/commands/new.py
+++ poetry/console/commands/new.py
@@ -1,3 +1,5 @@
+import sys
+
 from cleo import argument
 from cleo import option
 
@@ -18,7 +20,7 @@ class NewCommand(Command):
     def handle(self):
         from poetry.layouts import layout
         from poetry.utils._compat import Path
-        from poetry.utils.env import EnvManager
+        from poetry.utils.env import SystemEnv
         from poetry.vcs.git import GitConfig
 
         if self.option("src"):
@@ -49,7 +51,7 @@ class NewCommand(Command):
             if author_email:
                 author += " <{}>".format(author_email)
 
-        current_env = EnvManager().get(Path.cwd())
+        current_env = SystemEnv(Path(sys.executable))
         default_python = "^{}".format(
             ".".join(str(v) for v in current_env.version_info[:2])
         )
