02e266c1cfcfa276b3c76c863784f041c3251b4d
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2019-10-27 20:35:20 +0000

    Added logic to PipInstaller for FindLinksRepository

--- poetry/installation/pip_installer.py
+++ poetry/installation/pip_installer.py
@@ -52,21 +52,24 @@ class PipInstaller(BaseInstaller):
                 )
                 args += ["--trusted-host", parsed.hostname]
 
+
             if repository.cert:
                 args += ["--cert", str(repository.cert)]
 
             if repository.client_cert:
                 args += ["--client-cert", str(repository.client_cert)]
 
-            index_url = repository.authenticated_url
-
-            args += ["--index-url", index_url]
-            if self._pool.has_default():
-                if repository.name != self._pool.repositories[0].name:
-                    args += [
-                        "--extra-index-url",
-                        self._pool.repositories[0].authenticated_url,
-                    ]
+            if package.source_type == "legacy":
+                index_url = repository.authenticated_url
+                args += ["--index-url", index_url]
+                if self._pool.has_default():
+                    if repository.name != self._pool.repositories[0].name:
+                        args += [
+                            "--extra-index-url",
+                            self._pool.repositories[0].authenticated_url,
+                        ]
+            else:
+                args += ["--find-links", repository.url + "/" + repository.index_page]
 
         if update:
             args.append("-U")
