779fecd04cdb41672284974dd8beda3867e33a96
Sébastien Eustace <sebastien@eustace.io>
2019-11-12 19:08:50 +0100

    Fix custom indices dependency being constantly updated (#1563)

--- poetry/repositories/legacy_repository.py
+++ poetry/repositories/legacy_repository.py
@@ -269,7 +269,6 @@ class LegacyRepository(PyPiRepository):
 
         for version in versions:
             package = Package(name, version)
-            package.source_type = "legacy"
             package.source_url = self._url
 
             if extras is not None:
@@ -314,7 +313,6 @@ class LegacyRepository(PyPiRepository):
             if release_info["requires_python"]:
                 package.python_versions = release_info["requires_python"]
 
-            package.source_type = "legacy"
             package.source_url = self._url
             package.source_reference = self.name
 
