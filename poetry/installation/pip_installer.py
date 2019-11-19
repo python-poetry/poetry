319d8b937d1a9b179f6673148fc2c1a752f9670d
K900 <me@0upti.me>
2019-11-20 00:08:36 +0300

    commonize pip running logic and use system pip in system env (#861)

--- poetry/installation/pip_installer.py
+++ poetry/installation/pip_installer.py
@@ -115,7 +115,7 @@ class PipInstaller(BaseInstaller):
             raise
 
     def run(self, *args, **kwargs):  # type: (...) -> str
-        return self._env.run("python", "-m", "pip", *args, **kwargs)
+        return self._env.run_pip(*args, **kwargs)
 
     def requirement(self, package, formatted=False):
         if formatted and not package.source_type:
