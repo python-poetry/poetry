75964d616054679edb45c5904abe947e48ce32af
Max W Chase <max.chase@gmail.com>
2019-11-21 22:45:21 -0500

    Add a basic test that dev-only doesn't interact with the builder

--- tests/masonry/builders/test_builder.py
+++ tests/masonry/builders/test_builder.py
@@ -112,6 +112,60 @@ def test_get_metadata_content():
     ]
 
 
+def test_get_metadata_content_dev_only():
+    builder = Builder(
+        Factory().create_poetry(
+            Path(__file__).parent / "fixtures" / "complete_dev_only"
+        ),
+        NullEnv(),
+        NullIO(),
+    )
+
+    metadata = builder.get_metadata_content()
+
+    p = Parser()
+    parsed = p.parsestr(metadata)
+
+    assert parsed["Metadata-Version"] == "2.1"
+    assert parsed["Name"] == "my-package"
+    assert parsed["Version"] == "1.2.3"
+    assert parsed["Summary"] == "Some description."
+    assert parsed["Author"] == "Sébastien Eustace"
+    assert parsed["Author-email"] == "sebastien@eustace.io"
+    assert parsed["Keywords"] == "packaging,dependency,poetry"
+    assert parsed["Requires-Python"] == ">=3.6,<4.0"
+    assert parsed["License"] == "MIT"
+    assert parsed["Home-page"] == "https://poetry.eustace.io/"
+
+    classifiers = parsed.get_all("Classifier")
+    assert classifiers == [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+
+    extras = parsed.get_all("Provides-Extra")
+    assert extras == ["time"]
+
+    requires = parsed.get_all("Requires-Dist")
+    assert requires == [
+        "cachy[msgpack] (>=0.2.0,<0.3.0)",
+        "cleo (>=0.6,<0.7)",
+        'pendulum (>=1.4,<2.0); (python_version ~= "2.7" and sys_platform == "win32" or python_version in "3.4 3.5") and (extra == "time")',
+    ]
+
+    urls = parsed.get_all("Project-URL")
+    assert urls == [
+        "Documentation, https://poetry.eustace.io/docs",
+        "Issue Tracker, https://github.com/sdispater/poetry/issues",
+        "Repository, https://github.com/sdispater/poetry",
+    ]
+
+
 def test_metadata_homepage_default():
     builder = Builder(
         Factory().create_poetry(Path(__file__).parent / "fixtures" / "simple_version"),
