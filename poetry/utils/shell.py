cf7c5c16ec8fc2cddef1317069aa902566600e76
Sébastien Eustace <sebastien@eustace.io>
2019-11-22 15:04:05 +0100

    Fix shell command for bash

--- poetry/utils/shell.py
+++ poetry/utils/shell.py
@@ -58,7 +58,9 @@ class Shell:
                 self._path, ["-i"], dimensions=(terminal.height, terminal.width)
             )
 
-        c.setecho(False)
+        if not self._name == "bash":
+            c.setecho(False)
+
         activate_script = self._get_activate_script()
         bin_dir = "Scripts" if WINDOWS else "bin"
         activate_path = env.path / bin_dir / activate_script
