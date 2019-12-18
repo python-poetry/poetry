916598e4059a0831e5da003ba2cb849facb72a63
finswimmer77@gmail.com <GiWahnsinn07!>
2019-12-18 13:53:16 +0100

    change (get-poetry.py): changed hint about uninstalling poetry

--- get-poetry.py
+++ get-poetry.py
@@ -226,6 +226,9 @@ It will add the `poetry` command to {poetry}'s bin directory, located at:
 {poetry_home_bin}
 
 {platform_msg}
+
+You can uninstall at any time with by executing this script with the --uninstall option,
+and these changes will be reverted.
 """
 
 PRE_UNINSTALL_MESSAGE = """# We are sorry to see you go!
