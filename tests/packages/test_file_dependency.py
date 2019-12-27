e35487b1bcec7c0fee8436d33f07c0901e71113c
finswimmer77@gmail.com <GiWahnsinn07!>
2019-12-27 16:10:16 +0100

    fix (directory_dependency, file_dependency): create PEP508 compliant dependency string for directory and file dependencies

--- tests/packages/test_file_dependency.py
+++ tests/packages/test_file_dependency.py
@@ -3,8 +3,15 @@ import pytest
 from poetry.packages import FileDependency
 from poetry.utils._compat import Path
 
-
 DIST_PATH = Path(__file__).parent.parent / "fixtures" / "distributions"
+
+
+def test_file_dependency():
+    dependency = FileDependency("demo", DIST_PATH / "demo-0.1.0.tar.gz")
+
+    assert dependency.pretty_name == "demo"
+    assert dependency.path == DIST_PATH / "demo-0.1.0.tar.gz"
+    assert dependency.base_pep_508_name == "demo @ {}".format(DIST_PATH / "demo-0.1.0.tar.gz")
 
 
 def test_file_dependency_wrong_path():
