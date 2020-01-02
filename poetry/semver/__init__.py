python-poetry/poetry

commit c55d55a888538dbe8ae0b650997db6f9ef4d0aa9

Fix handling of PEP 440 ~= version constraint

poetry/semver/__init__.py
@@ -4,6 +4,7 @@ from .empty_constraint import EmptyConstraint
 from .patterns import BASIC_CONSTRAINT
 from .patterns import CARET_CONSTRAINT
 from .patterns import TILDE_CONSTRAINT
+from .patterns import TILDE_PEP440_CONSTRAINT
 from .patterns import X_CONSTRAINT
 from .version import Version
 from .version_constraint import VersionConstraint
@@ -59,6 +60,28 @@ def parse_single_constraint(constraint):  # type: (str) -> VersionConstraint
             high = version.stable.next_major
 
         return VersionRange(version, high, include_min=True)
+        return VersionRange()
+
+    # PEP 440 Tilde range (~=)
+    m = TILDE_PEP440_CONSTRAINT.match(constraint)
+    if m:
+        precision = 1
+        if m.group(3):
+            precision += 1
+
+            if m.group(4):
+                precision += 1
+
+        version = Version.parse(m.group(1))
+
+        if precision == 2:
+            low = version
+            high = version.stable.next_major
+        else:
+            low = Version(version.major, version.minor, 0)
+            high = version.stable.next_minor
+
+        return VersionRange(low, high, include_min=True)
 
     # Caret range
     m = CARET_CONSTRAINT.match(constraint)

tests/semver/test_main.py
@@ -60,6 +60,9 @@ def test_parse_constraint_wildcard(input, constraint):
         ("~1.2-beta", VersionRange(Version(1, 2, 0, "beta"), Version(1, 3, 0), True)),
         ("~1.2-b2", VersionRange(Version(1, 2, 0, "b2"), Version(1, 3, 0), True)),
         ("~0.3", VersionRange(Version(0, 3, 0), Version(0, 4, 0), True)),
+        ("~3.5", VersionRange(Version(3, 5, 0), Version(3, 6, 0), True)),
+        ("~=3.5", VersionRange(Version(3, 5, 0), Version(4, 0, 0), True)),  # PEP 440
+        ("~=3.5.3", VersionRange(Version(3, 5, 0), Version(3, 6, 0), True)),  # PEP 440
     ],
 )
 def test_parse_constraint_tilde(input, constraint):

poetry/semver/patterns.py
@@ -11,7 +11,8 @@ _COMPLETE_VERSION = "v?(\d+)(?:\.(\d+))?(?:\.(\d+))?{}(?:\+[^\s]+)?".format(MODI
 COMPLETE_VERSION = re.compile("(?i)" + _COMPLETE_VERSION)
 
 CARET_CONSTRAINT = re.compile("(?i)^\^({})$".format(_COMPLETE_VERSION))
-TILDE_CONSTRAINT = re.compile("(?i)^~=?({})$".format(_COMPLETE_VERSION))
+TILDE_CONSTRAINT = re.compile("(?i)^~(?!=)({})$".format(_COMPLETE_VERSION))
+TILDE_PEP440_CONSTRAINT = re.compile("(?i)^~=({})$".format(_COMPLETE_VERSION))
 X_CONSTRAINT = re.compile("^(!= ?|==)?v?(\d+)(?:\.(\d+))?(?:\.(\d+))?(?:\.[xX*])+$")
 BASIC_CONSTRAINT = re.compile(
     "(?i)^(<>|!=|>=?|<=?|==?)?\s*({})".format(_COMPLETE_VERSION)

commit 51d66ce15baf65549b9975b1aa6946df4520bdc3

Fix PEP440 compatible release (`~=`) handling
PEP440 compatible release version range (`~=`) should have itself as a minimum
value. Examples from [official
doc](https://python.org/dev/peps/pep-0440/#compatible-release) (each pair is
equivalent):

```
~=2.2
>=2.2, ==2.*

~=1.4.5
>=1.4.5, ==1.4.*

~=2.2.post3
>=2.2.post3, ==2.*

~=1.4.5a4
>=1.4.5a4, ==1.4.*
```

In every case, lower bound is exactly same as the input version. Current
incorrect behavior was originally introduced 2 years ago by commit c55d55a8,
where it tried to reset patch version to 0 for tilde expression.

Closes #1150.

poetry/semver/__init__.py
@@ -76,14 +76,12 @@ def parse_single_constraint(constraint):  # type: (str) -> VersionConstraint
         version = Version.parse(m.group(1))
 
         if precision == 2:
-            low = version
             high = version.stable.next_major
         else:
-            low = Version(version.major, version.minor, version.patch)
             high = version.stable.next_minor
 
         return VersionRange(
-            low, high, include_min=True, always_include_max_prerelease=True
+            version, high, include_min=True, always_include_max_prerelease=True
         )
 
     # Caret range

tests/semver/test_main.py
@@ -68,6 +68,7 @@ def test_parse_constraint_wildcard(input, constraint):
         ("~3.5", VersionRange(Version(3, 5, 0), Version(3, 6, 0), True)),
         ("~=3.5", VersionRange(Version(3, 5, 0), Version(4, 0, 0), True)),  # PEP 440
         ("~=3.5.3", VersionRange(Version(3, 5, 3), Version(3, 6, 0), True)),  # PEP 440
+        ("~=3.5.3rc1", VersionRange(Version(3, 5, 3, pre='rc1'), Version(3, 6, 0), True)),  # PEP 440
     ],
 )
 def test_parse_constraint_tilde(input, constraint):