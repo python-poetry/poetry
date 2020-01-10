10e471a0e6a916acd4b6b1bf5362a0d7054ebc04
finswimmer <finswimmer77@gmail.com>
2020-01-10 10:07:02 +0100

    create PEP508 compliant dependency string for directory and file dependencies (#1796)
    * fix (directory_dependency, file_dependency): create PEP508 compliant dependency string for directory and file dependencies
    
    * fix (directory_dependency, file_dependency): making isort and black happy

--- tests/packages/test_directory_dependency.py
+++ tests/packages/test_directory_dependency.py
@@ -8,12 +8,24 @@ from poetry.utils.env import EnvCommandError
 from poetry.utils.env import MockEnv as BaseMockEnv
 
 
+fixtures_dir = Path(__file__).parent.parent / "fixtures"
+DIST_PATH = Path(__file__).parent.parent / "fixtures" / "git" / "github.com" / "demo"
+
+
 class MockEnv(BaseMockEnv):
     def run(self, bin, *args):
         raise EnvCommandError(CalledProcessError(1, "python", output=""))
 
 
-DIST_PATH = Path(__file__).parent.parent / "fixtures" / "git" / "github.com" / "demo"
+def test_directory_dependency():
+    dependency = DirectoryDependency("simple_project", fixtures_dir / "simple_project")
+
+    assert dependency.pretty_name == "simple_project"
+    assert dependency.develop
+    assert dependency.path == fixtures_dir / "simple_project"
+    assert dependency.base_pep_508_name == "simple_project @ {}".format(
+        fixtures_dir / "simple_project"
+    )
 
 
 def test_directory_dependency_must_exist():
