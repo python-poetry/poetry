e66a53e0d0f5127494d9321bd2632b6e5928a243
Tom Milligan <tom@reinfer.io>
2020-04-01 14:14:08 +0100

    package: allow url dependencies to be optional

--- tests/packages/test_package.py
+++ tests/packages/test_package.py
@@ -49,3 +49,20 @@ def test_package_add_dependency_vcs_category_default_main():
         "poetry", constraint={"git": "https://github.com/python-poetry/poetry.git"}
     )
     assert dependency.category == "main"
+
+
+@pytest.mark.parametrize("category", ["main", "dev"])
+@pytest.mark.parametrize("optional", [True, False])
+def test_package_url_category_optional(category, optional):
+    package = Package("foo", "0.1.0")
+
+    dependency = package.add_dependency(
+        "poetry",
+        constraint={
+            "url": "https://github.com/python-poetry/poetry/releases/download/1.0.5/poetry-1.0.5-linux.tar.gz",
+            "optional": optional,
+        },
+        category=category,
+    )
+    assert dependency.category == category
+    assert dependency.is_optional() == optional
