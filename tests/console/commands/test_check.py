f8bde0e3bc0aae4b86309ffc7008b8a07375fa9d
Sébastien Eustace <sebastien@eustace.io>
2020-04-24 11:27:54 +0200

    Update tests for the check command

--- tests/console/commands/test_check.py
+++ tests/console/commands/test_check.py
@@ -34,14 +34,12 @@ def test_check_invalid(app, mocker):
     if PY2:
         expected = """\
 Error: u'description' is a required property
-Error: INVALID is not a valid license
 Warning: A wildcard Python dependency is ambiguous. Consider specifying a more explicit one.
 Warning: The "pendulum" dependency specifies the "allows-prereleases" property, which is deprecated. Use "allow-prereleases" instead.
 """
     else:
         expected = """\
 Error: 'description' is a required property
-Error: INVALID is not a valid license
 Warning: A wildcard Python dependency is ambiguous. Consider specifying a more explicit one.
 Warning: The "pendulum" dependency specifies the "allows-prereleases" property, which is deprecated. Use "allow-prereleases" instead.
 """
