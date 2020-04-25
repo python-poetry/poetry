844c57789ba21a8d26f19797f6b61ba2e20d8a9b
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2020-04-25 13:49:28 +0200

    Handle whitespaces for PEP-440 constraints
    * fix incorrect parsing of spaces when parsing version constraints
    * add tests for `parse_constraint`

--- poetry/semver/patterns.py
+++ poetry/semver/patterns.py
@@ -14,8 +14,8 @@ _COMPLETE_VERSION = r"v?(\d+)(?:\.(\d+))?(?:\.(\d+))?(?:\.(\d+))?{}(?:\+[^\s]+)?
 COMPLETE_VERSION = re.compile("(?i)" + _COMPLETE_VERSION)
 
 CARET_CONSTRAINT = re.compile(r"(?i)^\^({})$".format(_COMPLETE_VERSION))
-TILDE_CONSTRAINT = re.compile("(?i)^~(?!=)({})$".format(_COMPLETE_VERSION))
-TILDE_PEP440_CONSTRAINT = re.compile("(?i)^~=({})$".format(_COMPLETE_VERSION))
+TILDE_CONSTRAINT = re.compile(r"(?i)^~(?!=)\s*({})$".format(_COMPLETE_VERSION))
+TILDE_PEP440_CONSTRAINT = re.compile(r"(?i)^~=\s*({})$".format(_COMPLETE_VERSION))
 X_CONSTRAINT = re.compile(r"^(!=|==)?\s*v?(\d+)(?:\.(\d+))?(?:\.(\d+))?(?:\.[xX*])+$")
 BASIC_CONSTRAINT = re.compile(
     r"(?i)^(<>|!=|>=?|<=?|==?)?\s*({}|dev)".format(_COMPLETE_VERSION)
