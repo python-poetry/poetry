127b08378c6849184373baca4024358ed2bcae86
Sébastien Eustace <sebastien@eustace.io>
2020-06-02 18:47:58 +0200

    Fix legacy repositories information not being set on packages

--- poetry/repositories/legacy_repository.py
+++ poetry/repositories/legacy_repository.py
@@ -269,6 +269,8 @@ class LegacyRepository(PyPiRepository):
 
         for version in versions:
             package = Package(name, version)
+            package.source_type = "legacy"
+            package.source_reference = self.name
             package.source_url = self._url
 
             if extras is not None:
@@ -313,6 +315,7 @@ class LegacyRepository(PyPiRepository):
             if release_info["requires_python"]:
                 package.python_versions = release_info["requires_python"]
 
+            package.source_type = "legacy"
             package.source_url = self._url
             package.source_reference = self.name
 
