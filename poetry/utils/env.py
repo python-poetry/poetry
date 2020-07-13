164c5baa28e35d17bdee7f62c39655fe9416b32b
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2020-07-13 23:41:45 +0200

    Standardise on using virtualenv package
    Until now poetry used the built-in venv module available in Python 3.
    This has presented a few concerns. Chief of which has been
    inconsistent environment setup. This includes, but is not limited to,
    pip version used by the `ensurepip` module which is in turn used by the
    built-in `venv.EnvBuilder`. Additionally, the `virtualenv` package
    allows for faster environment creation. This can also allow us to, going
    forward, drop ad-hoc code required to inspect and manage environments.

--- poetry/utils/env.py
+++ poetry/utils/env.py
@@ -15,12 +15,14 @@ from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Tuple
+from typing import Union
 
 import tomlkit
 
 from clikit.api.io import IO
 
 import packaging.tags
+import virtualenv
 
 from packaging.tags import Tag
 from packaging.tags import interpreter_name
@@ -139,27 +141,6 @@ import json
 import sysconfig
 
 print(json.dumps(sysconfig.get_paths()))
-"""
-
-CREATE_VENV_COMMAND = """\
-path = {!r}
-
-try:
-    from venv import EnvBuilder
-
-    builder = EnvBuilder(with_pip=True)
-    builder.create(path)
-except ImportError:
-    try:
-        # We fallback on virtualenv for Python 2.7
-        from virtualenv import create_environment
-
-        create_environment(path)
-    except ImportError:
-        # since virtualenv>20 we have to use cli_run
-        from virtualenv import cli_run
-
-        cli_run([path])
 """
 
 
@@ -697,43 +678,20 @@ class EnvManager(object):
         return VirtualEnv(venv)
 
     @classmethod
-    def build_venv(cls, path, executable=None):
-        if executable is not None:
-            # Create virtualenv by using an external executable
-            try:
-                p = subprocess.Popen(
-                    list_to_shell_command([executable, "-"]),
-                    stdin=subprocess.PIPE,
-                    shell=True,
-                )
-                p.communicate(encode(CREATE_VENV_COMMAND.format(path)))
-            except CalledProcessError as e:
-                raise EnvCommandError(e)
-
-            return
-
-        try:
-            from venv import EnvBuilder
-
-            # use the same defaults as python -m venv
-            if os.name == "nt":
-                use_symlinks = False
-            else:
-                use_symlinks = True
-
-            builder = EnvBuilder(with_pip=True, symlinks=use_symlinks)
-            builder.create(path)
-        except ImportError:
-            try:
-                # We fallback on virtualenv for Python 2.7
-                from virtualenv import create_environment
-
-                create_environment(path)
-            except ImportError:
-                # since virtualenv>20 we have to use cli_run
-                from virtualenv import cli_run
-
-                cli_run([path])
+    def build_venv(
+        cls, path, executable=None
+    ):  # type: (str, Optional[Union[str, Path]]) -> virtualenv.run.session.Session
+        if isinstance(executable, Path):
+            executable = executable.resolve()
+        return virtualenv.cli_run(
+            [
+                "--no-download",
+                "--no-periodic-update",
+                "--python",
+                executable or sys.executable,
+                path,
+            ]
+        )
 
     def remove_venv(self, path):  # type: (str) -> None
         shutil.rmtree(path)
