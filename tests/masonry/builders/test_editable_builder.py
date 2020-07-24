8d54665a2fef54e957eb6980857d6d62306adfe4
Sébastien Eustace <sebastien@eustace.io>
2020-07-24 16:33:00 +0200

    Fix tests following the upgrade of poetry-core

--- tests/masonry/builders/test_editable_builder.py
+++ tests/masonry/builders/test_editable_builder.py
@@ -105,6 +105,7 @@ Classifier: Programming Language :: Python :: 3.5
 Classifier: Programming Language :: Python :: 3.6
 Classifier: Programming Language :: Python :: 3.7
 Classifier: Programming Language :: Python :: 3.8
+Classifier: Programming Language :: Python :: 3.9
 Classifier: Topic :: Software Development :: Build Tools
 Classifier: Topic :: Software Development :: Libraries :: Python Modules
 Project-URL: Documentation, https://python-poetry.org/docs
