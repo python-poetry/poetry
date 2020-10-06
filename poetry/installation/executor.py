a74310374e4d932a8565db07619672dca08f32b0
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2020-10-06 11:38:04 +0200

    executor: execute parallel unsafe ops serially
    Resolves: #2658 #3086

--- poetry/installation/executor.py
+++ poetry/installation/executor.py
@@ -107,14 +107,33 @@ class Executor(object):
         self._sections = OrderedDict()
         for _, group in groups:
             tasks = []
+            serial_operations = []
             for operation in group:
                 if self._shutdown:
                     break
+
+                # Some operations are unsafe, we mus execute them serially in a group
+                # https://github.com/python-poetry/poetry/issues/3086
+                # https://github.com/python-poetry/poetry/issues/2658
+                #
+                # We need to explicitly check source type here, see:
+                # https://github.com/python-poetry/poetry-core/pull/98
+                is_parallel_unsafe = operation.job_type == "uninstall" or (
+                    operation.package.develop
+                    and operation.package.source_type in {"directory", "git"}
+                )
+                if not operation.skipped and is_parallel_unsafe:
+                    serial_operations.append(operation)
+                    continue
 
                 tasks.append(self._executor.submit(self._execute_operation, operation))
 
             try:
                 wait(tasks)
+
+                for operation in serial_operations:
+                    wait([self._executor.submit(self._execute_operation, operation)])
+
             except KeyboardInterrupt:
                 self._shutdown = True
 
