67bc2d9efdbc899f223ccfa641e65a15fe742a12
Paul Sanders <psanders1@gmail.com>
2020-10-10 21:09:49 -0400

    Relates-to: #3155 converting duplicted code to fixture

--- tests/console/commands/debug/test_resolve.py
+++ tests/console/commands/debug/test_resolve.py
@@ -9,7 +9,8 @@ def tester(command_tester_factory):
     return command_tester_factory("debug resolve")
 
 
-def test_debug_resolve_gives_resolution_results(tester, repo):
+@pytest.fixture()
+def cachy_repo(repo):
     cachy2 = get_package("cachy", "0.2.0")
     cachy2.add_dependency(Factory.create_dependency("msgpack-python", ">=0.5 <0.6"))
 
@@ -17,6 +18,8 @@ def test_debug_resolve_gives_resolution_results(tester, repo):
     repo.add_package(cachy2)
     repo.add_package(get_package("msgpack-python", "0.5.3"))
 
+
+def test_debug_resolve_gives_resolution_results(tester, cachy_repo):
     tester.execute("cachy")
 
     expected = """\
@@ -31,14 +34,7 @@ cachy          0.2.0
     assert expected == tester.io.fetch_output()
 
 
-def test_debug_resolve_tree_option_gives_the_dependency_tree(tester, repo):
-    cachy2 = get_package("cachy", "0.2.0")
-    cachy2.add_dependency(Factory.create_dependency("msgpack-python", ">=0.5 <0.6"))
-
-    repo.add_package(get_package("cachy", "0.1.0"))
-    repo.add_package(cachy2)
-    repo.add_package(get_package("msgpack-python", "0.5.3"))
-
+def test_debug_resolve_tree_option_gives_the_dependency_tree(tester, cachy_repo):
     tester.execute("cachy --tree")
 
     expected = """\
