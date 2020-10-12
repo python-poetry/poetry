01609955b846cbb1453fca7519e1099cbf1abd06
Paul Sanders <psanders1@gmail.com>
2020-10-12 09:27:15 -0400

    Adding additional packages to fixture and making it autouse

--- tests/console/commands/debug/test_resolve.py
+++ tests/console/commands/debug/test_resolve.py
@@ -9,8 +9,8 @@ def tester(command_tester_factory):
     return command_tester_factory("debug resolve")
 
 
-@pytest.fixture()
-def cachy_repo(repo):
+@pytest.fixture(autouse=True)
+def __add_packages(repo):
     cachy2 = get_package("cachy", "0.2.0")
     cachy2.add_dependency(Factory.create_dependency("msgpack-python", ">=0.5 <0.6"))
 
@@ -18,8 +18,11 @@ def cachy_repo(repo):
     repo.add_package(cachy2)
     repo.add_package(get_package("msgpack-python", "0.5.3"))
 
+    repo.add_package(get_package("pendulum", "2.0.3"))
+    repo.add_package(get_package("cleo", "0.6.5"))
 
-def test_debug_resolve_gives_resolution_results(tester, cachy_repo):
+
+def test_debug_resolve_gives_resolution_results(tester):
     tester.execute("cachy")
 
     expected = """\
@@ -34,7 +37,7 @@ cachy          0.2.0
     assert expected == tester.io.fetch_output()
 
 
-def test_debug_resolve_tree_option_gives_the_dependency_tree(tester, cachy_repo):
+def test_debug_resolve_tree_option_gives_the_dependency_tree(tester):
     tester.execute("cachy --tree")
 
     expected = """\
@@ -49,10 +52,7 @@ cachy 0.2.0
     assert expected == tester.io.fetch_output()
 
 
-def test_debug_resolve_git_dependency(tester, repo):
-    repo.add_package(get_package("pendulum", "2.0.3"))
-    repo.add_package(get_package("cleo", "0.6.5"))
-
+def test_debug_resolve_git_dependency(tester):
     tester.execute("git+https://github.com/demo/demo.git")
 
     expected = """\
