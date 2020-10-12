8f19f8c5fa81265e4b3153c1527edeed95aabee5
KGB33 <github@bassingthwaite.org>
2020-10-12 08:47:24 -0600

    added some tests for Shell.get()

--- tests/utils/test_shell.py
+++ tests/utils/test_shell.py
@@ -1,10 +1,94 @@
-from poetry.utils.shell import Shell
+import pytest
+
+from poetry.utils.shell import Shell as _Shell
+from poetry.utils.shell import os
 
 
-def test_name_and_path_properties():
+@pytest.fixture
+def Shell():
+    _Shell._shell = None
+    return _Shell
+
+
+def test_name_and_path_properties(Shell):
     """
-    Given a Shell object, check that???
+    Given a Shell object,
+    Check that the name and path propterites
+    match the _name and _path attributes,
+    as well as the values passed in.
     """
     s = Shell(name="name", path="path")
     assert s.name == s._name == "name"
     assert s.path == s._path == "path"
+
+
+def test_default_shell_is_None(Shell):
+    """
+    Given the Shell class, check that _shell is None
+    """
+    assert Shell._shell is None
+
+
+def test_get_when__shell_is_not_none(Shell):
+    """
+    Given the Shell class, when _shell is not None,
+    Check that the get class method returns Shell._shell
+    """
+    s = Shell(name="name", path="path")
+    Shell._shell = s
+    assert Shell.get() == s
+
+
+def test_get_when_detect_shell_works(Shell, mocker):
+    """
+    Given the Shell class,
+    When Shell.get() is called, and shellingham.detect_shell()
+        doesn't error.
+    Check that the resulting shell's name and path are as expected,
+        and that Shell._shell is updated.
+    """
+    mocker.patch(
+        "poetry.utils.shell.detect_shell", return_value=("Mocked Name", "Mocked Path")
+    )
+    s = Shell.get()
+    assert s.name == "Mocked Name"
+    assert s.path == "Mocked Path"
+    assert Shell._shell == s
+
+
+@pytest.mark.skipif(os.name != "posix", reason="TODO")
+def test_get_when_detect_shell_raises_error_posix(Shell, mocker):
+    """
+    Given the Shell Class running on a posix system.
+    When Shell.get() is called, and shellingham.detect_shell()
+        raises an error.
+    Check that the resulting shell is as expected.
+    """
+    mocker.patch("poetry.utils.shell.detect_shell", side_effect=RuntimeError)
+    mocker.patch(
+        "poetry.utils.shell.os.environ.get", return_value="/blah/blah/blah/name"
+    )
+
+    s = Shell.get()
+    assert s.name == "name"
+    assert s.path == "/blah/blah/blah/name"
+    assert Shell._shell == s
+
+
+@pytest.mark.skipif(os.name != "nt", reason="TODO")
+def test_get_when_detect_shell_raises_error_nt(Shell, mocker):
+    """
+    Given the Shell Class running on a nt system.
+    When Shell.get() is called, and shellingham.detect_shell()
+        raises an error.
+    Check that the resulting shell is as expected.
+    """
+    mocker.patch("poetry.utils.shell.detect_shell", side_effect=RuntimeError)
+    mocker.patch(
+        "poetry.utils.shell.os.environ.get", return_value=r"\blah\blah\blah\name"
+    )
+
+    s = Shell.get()
+    assert s.name == "name"
+    assert s.path == r"\blah\blah\blah\name"
+    assert Shell._shell == s
