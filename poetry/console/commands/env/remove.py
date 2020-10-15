56332636be642f772ba96e4589d8f83bd6b126bc
DustinMoriarty <dustin.moriarty@protonmail.com>
2020-10-15 00:21:17 -0500

    Added suppor for removing multiple virtualenvs in CLI.

--- poetry/console/commands/env/remove.py
+++ poetry/console/commands/env/remove.py
@@ -1,4 +1,4 @@
-from cleo import argument
+from cleo import argument, option
 
 from ..command import Command
 
@@ -6,16 +6,26 @@ from ..command import Command
 class EnvRemoveCommand(Command):
 
     name = "remove"
-    description = "Removes a specific virtualenv associated with the project."
+    description = "Removes specific virtualenvs associated with the project."
 
     arguments = [
-        argument("python", "The python executable to remove the virtualenv for.")
+        argument("python", "The python executables to remove the virtualenv for.", optional=True, multiple=True)
+    ]
+    options = [
+        option("all", description="Remove all virtualenv associated with the project." ),
     ]
 
     def handle(self):
         from poetry.utils.env import EnvManager
 
         manager = EnvManager(self.poetry)
-        venv = manager.remove(self.argument("python"))
+        pythons = self.argument("python")
+        all = self.option("all")
+        if all:
+            pythons = [x.path.name for x in manager.list()]
+        if not pythons:
+            self.line("No virtualenv provided.")
+        for python in pythons:
+            venv = manager.remove(python)
+            self.line("Deleted virtualenv: <comment>{}</comment>".format(venv.path))
 
-        self.line("Deleted virtualenv: <comment>{}</comment>".format(venv.path))
