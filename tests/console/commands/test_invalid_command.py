30bbc4b05da42b1ca66007de6458547b2f8847ae
cdeler <serj.krotov@gmail.com>
2020-10-24 22:33:43 +0300

    Checked that all the exceptions came from poetry (i.e. not from external libraries)

--- tests/console/commands/test_invalid_command.py
+++ tests/console/commands/test_invalid_command.py
@@ -10,5 +10,8 @@ def test_overwrite_exception_in_case_of_invalid_command(app):
     conf = ApplicationConfig()
     args = ArgvArgs(["poetry", "--vers"])
 
-    with pytest.raises(PoetryException):
+    with pytest.raises(PoetryException) as cm:
         conf.create_io(app, args)
+
+    stacktrace_files = [str(stack_frame.path) for stack_frame in cm.traceback]
+    assert all(("poetry" in path for path in stacktrace_files))
