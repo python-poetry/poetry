904d97cea8b4ce38369a29c3c4a6c2fe87f2eb4a
Rijken, M.A.M.M. (Marc) <marc.rijken@sns.nl>
2020-11-04 20:28:32 +0100

    Lock dependencies on build

--- poetry/console/commands/build.py
+++ poetry/console/commands/build.py
@@ -9,7 +9,8 @@ class BuildCommand(EnvCommand):
     description = "Builds a package, as a tarball and a wheel by default."
 
     options = [
-        option("format", "f", "Limit the format to either sdist or wheel.", flag=False)
+        option("format", "f", "Limit the format to either sdist or wheel.", flag=False),
+        option("lock", "l", "Add all dependencies as locked dependencies in the distributed pyproject.toml", flag=True)
     ]
 
     loggers = [
@@ -25,6 +26,8 @@ class BuildCommand(EnvCommand):
         if self.option("format"):
             fmt = self.option("format")
 
+        lock = self.option("lock")
+
         package = self.poetry.package
         self.line(
             "Building <c1>{}</c1> (<c2>{}</c2>)".format(
@@ -32,5 +35,24 @@ class BuildCommand(EnvCommand):
             )
         )
 
+        original_content = self.poetry.file.read()
+        if lock:
+            section = "dependencies"
+
+            content = self.poetry.file.read()
+            poetry_content = content["tool"]["poetry"]
+            if section not in poetry_content:
+                poetry_content[section] = {}
+
+            for dependency_package in self._poetry.locker.lock_data["package"]:
+                name = dependency_package["name"]
+                version = dependency_package["version"]
+                poetry_content[section][name] = version
+
+            self.poetry.file.write(content)
+
         builder = Builder(self.poetry)
         builder.build(fmt, executable=self.env.python)
+
+        if lock:
+            self.poetry.file.write(original_content)
