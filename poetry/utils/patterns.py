42b364ebdbd3fb7e9273fcd968254219167d30f1
finswimmer <finswimmer77@gmail.com>
2020-11-07 15:42:57 +0100

    fix (utils.patterns): recognize one digit version in filename fix (utils.patterns): version part in filename must be present according PEP491

--- poetry/utils/patterns.py
+++ poetry/utils/patterns.py
@@ -2,8 +2,11 @@ import re
 
 
 wheel_file_re = re.compile(
-    r"""^(?P<namever>(?P<name>.+?)(-(?P<ver>\d.+?))?)
-        ((-(?P<build>\d.*?))?-(?P<pyver>.+?)-(?P<abi>.+?)-(?P<plat>.+?)
-        \.whl|\.dist-info)$""",
+    r"^(?P<namever>(?P<name>.+?)-(?P<ver>\d.*?))"
+    r"(-(?P<build>\d.*?))?"
+    r"-(?P<pyver>.+?)"
+    r"-(?P<abi>.+?)"
+    r"-(?P<plat>.+?)"
+    r"\.whl|\.dist-info$",
     re.VERBOSE,
 )
