python-poetry/poetry

commit d22afa9d933529c01b74a6127fa41ce49bf0182b

fix: Gracefully handle read-only sdists
For a package without dependencies in pypi's database, like p4python,
the sdist is required.  The problem is p4python was developed in
perforce where all files are read-only by default and deleting the temp
directory fails.

So we need to use the custom-built temp directory and specially handle
read-only files to be able to use p4python in poetry.

Fixes #520

poetry/utils/helpers.py
@@ -34,19 +34,18 @@ def normalize_version(version):  # type: (str) -> str
     return str(Version(version))
 
 
+def _del_ro(action, name, exc):
+    os.chmod(name, stat.S_IWRITE)
+    os.remove(name)
+
+
 @contextmanager
 def temporary_directory(*args, **kwargs):
-    try:
-        from tempfile import TemporaryDirectory
+    name = tempfile.mkdtemp(*args, **kwargs)
 
-        with TemporaryDirectory(*args, **kwargs) as name:
-            yield name
-    except ImportError:
-        name = tempfile.mkdtemp(*args, **kwargs)
+    yield name
 
-        yield name
-
-        shutil.rmtree(name)
+    shutil.rmtree(name, onerror=_del_ro)
 
 
 def parse_requires(requires):  # type: (str) -> List[str]

commit 747375b2e03093d191363802cf60285cf2f2c21a

utils: Fix FileNotFoundError in temporary_directory()
Don't try to delete temporary directories that no longer exist.

Fixes: d22afa9d933529c01b74a6127fa41ce49bf0182b

poetry/utils/helpers.py
@@ -48,7 +48,8 @@ def temporary_directory(*args, **kwargs):
 
     yield name
 
-    shutil.rmtree(name, onerror=_del_ro)
+    if os.path.exists(name):
+        shutil.rmtree(name, onerror=_del_ro)
 
 
 def get_cert(config, repository_name):  # type: (Config, str) -> Optional[Path]