7dcb682a009883042eecdf5c053febc611dccfd0
Tucker Beck <tucker.beck@officeally.com>
2021-02-03 17:49:59 -0800

    Issue #3483: Fixed cert handling for pathlib paths

--- src/poetry/utils/authenticator.py
+++ src/poetry/utils/authenticator.py
@@ -73,6 +73,12 @@ class Authenticator:
         verify = kwargs.get("verify", certs.get("verify"))
         cert = kwargs.get("cert", certs.get("cert"))
 
+        if isinstance(cert, pathlib.PurePath):
+            cert = str(cert)
+
+        if isinstance(verify, pathlib.PurePath):
+            verify = str(verify)
+
         settings = session.merge_environment_settings(
             prepared_request.url, proxies, stream, verify, cert
         )
