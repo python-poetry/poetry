9753d0cdaa318761ff9526b34a5d7285e87981bb
DustinMoriarty <dustin.moriarty@protonmail.com>
2021-03-11 23:04:53 -0600

    Updated python argument description.

--- poetry/console/commands/env/remove.py
+++ poetry/console/commands/env/remove.py
@@ -12,7 +12,8 @@ class EnvRemoveCommand(Command):
     arguments = [
         argument(
             "python",
-            "The python executable(s) to remove the virtual environment for.",
+            "The python executables of the virtual environments which are to be "
+            "removed.",
             optional=True,
             multiple=True,
         )
