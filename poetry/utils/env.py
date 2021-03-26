5073c205e3a5c189a8bf0eae52a4c48ee1b88eb1
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2021-03-27 00:37:40 +0100

    env: align ephemeral environment interface to build

--- poetry/utils/env.py
+++ poetry/utils/env.py
@@ -1464,9 +1464,10 @@ class NullEnv(SystemEnv):
 @contextmanager
 def ephemeral_environment(
     executable=None,
-    pip: bool = False,
-    wheel: Optional[bool] = None,
-    setuptools: Optional[bool] = None,
+    flags: Dict[str, bool] = None,
+    with_pip: bool = False,
+    with_wheel: Optional[bool] = None,
+    with_setuptools: Optional[bool] = None,
 ) -> ContextManager[VirtualEnv]:
     with temporary_directory() as tmp_dir:
         # TODO: cache PEP 517 build environment corresponding to each project venv
@@ -1474,9 +1475,10 @@ def ephemeral_environment(
         EnvManager.build_venv(
             path=venv_dir.as_posix(),
             executable=executable,
-            with_pip=pip,
-            with_wheel=wheel,
-            with_setuptools=setuptools,
+            flags=flags,
+            with_pip=with_pip,
+            with_wheel=with_wheel,
+            with_setuptools=with_setuptools,
         )
         yield VirtualEnv(venv_dir, venv_dir)
 
