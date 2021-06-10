2f48c2d8465ca19d87142c82c151fa767a86c726
Ggicci <ggicci.t@gmail.com>
2021-06-10 11:32:36 +0800

    chore: remove unused variables in test_update

--- tests/console/commands/self/test_update.py
+++ tests/console/commands/self/test_update.py
@@ -84,15 +84,8 @@ def test_self_update_does_not_update_non_recommended_installation(
 
     new_version = Version.parse(__version__).next_minor().text
 
-    old_poetry = Package("poetry", __version__)
-    old_poetry.add_dependency(Factory.create_dependency("cleo", "^0.8.2"))
-
     new_poetry = Package("poetry", new_version)
     new_poetry.add_dependency(Factory.create_dependency("cleo", "^1.0.0"))
-
-    installed_repository = Repository()
-    installed_repository.add_package(old_poetry)
-    installed_repository.add_package(Package("cleo", "0.8.2"))
 
     repository = Repository()
     repository.add_package(new_poetry)
