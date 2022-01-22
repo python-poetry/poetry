773b4ec6dc5b25fbffa719cda2903fb3daab1b4d
Evan Rittenhouse <evan.rittenhouse@workboard.com>
2022-01-22 09:04:57 -0600

    Removed unnecessary helper function calls

--- src/poetry/console/commands/init.py
+++ src/poetry/console/commands/init.py
@@ -276,14 +276,11 @@ You can specify a package in the following forms:
                 else:
                     choices = []
                     matches_names = [p.name for p in matches]
-                    exact_match = canonicalize_name(constraint["name"]) in matches_names
+                    canonicalized_name = canonicalize_name(constraint["name"])
+                    exact_match = canonicalized_name in matches_names
                     if exact_match:
                         choices.append(
-                            matches[
-                                matches_names.index(
-                                    canonicalize_name(constraint["name"])
-                                )
-                            ].pretty_name
+                            matches[matches_names.index(canonicalized_name)].pretty_name
                         )
 
                     for found_package in matches:
