54d26b85bc331f6b8ed4300760691cad1f957477
Brian Marroquin <brianmarroq@gmail.com>
2022-02-01 20:12:14 -0800

    adds comments for mypy fix

--- src/poetry/utils/shell.py
+++ src/poetry/utils/shell.py
@@ -72,6 +72,8 @@ class Shell:
         bin_dir = "Scripts" if WINDOWS else "bin"
         activate_path = env.path / bin_dir / activate_script
 
+        # mypy requires using sys.platform instead of WINDOWS constant
+        # in if statements to properly type check on Windows
         if sys.platform == "win32":
             if self._name in ("powershell", "pwsh"):
                 args = ["-NoExit", "-File", str(activate_path)]
