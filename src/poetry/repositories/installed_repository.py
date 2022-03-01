195a8133482598f380e5205cbefccf0a845b2e12
Branch Vincent <branchevincent@gmail.com>
2022-02-28 21:58:07 -0500

    chore(repositories): remove type errors

--- src/poetry/repositories/installed_repository.py
+++ src/poetry/repositories/installed_repository.py
@@ -106,7 +106,7 @@ class InstalledRepository(Repository):
     ) -> Package:
         # We first check for a direct_url.json file to determine
         # the type of package.
-        path = Path(str(distribution._path))
+        path = Path(str(distribution._path))  # type: ignore[attr-defined]
 
         if (
             path.name.endswith(".dist-info")
@@ -172,7 +172,7 @@ class InstalledRepository(Repository):
 
     @classmethod
     def create_package_from_pep610(cls, distribution: metadata.Distribution) -> Package:
-        path = Path(str(distribution._path))
+        path = Path(str(distribution._path))  # type: ignore[attr-defined]
         source_type = None
         source_url = None
         source_reference = None
@@ -233,14 +233,14 @@ class InstalledRepository(Repository):
         for entry in reversed(env.sys_path):
             for distribution in sorted(
                 metadata.distributions(path=[entry]),
-                key=lambda d: str(d._path),
+                key=lambda d: str(d._path),  # type: ignore[attr-defined]
             ):
                 name = canonicalize_name(distribution.metadata["name"])
 
                 if name in seen:
                     continue
 
-                path = Path(str(distribution._path))
+                path = Path(str(distribution._path))  # type: ignore[attr-defined]
 
                 try:
                     path.relative_to(_VENDORS)
