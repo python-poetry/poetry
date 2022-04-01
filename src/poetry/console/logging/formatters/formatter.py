9b8d8304fa1e5b0cd9e7476d02c9dd8a27e5f4d3
Edwin Yllanes <e.yllanescucho@gmail.com>
2022-04-01 07:00:28 -0500

    chore(console): remove type errors (#5258)
    * chore(console/application): remove type errors
    
    * chore(console/logging): remove type errors
    
    * chore(utils/setup_reader): some fixes
    
    * Revert "chore(utils/setup_reader): some fixes"
    
    This reverts commit 9d82be920ea3d3c0fb0b818b29a1be3b82819b1c.

--- src/poetry/console/logging/formatters/formatter.py
+++ src/poetry/console/logging/formatters/formatter.py
@@ -1,12 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-
-if TYPE_CHECKING:
-    import logging
-
 
 class Formatter:
-    def format(self, record: logging.LogRecord) -> str:
+    def format(self, record: str) -> str:
         raise NotImplementedError()
