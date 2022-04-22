405c10dca82f42e67e88823bf2962ba14a2b0311
Bernardo Meurer <bernardo@meurer.org>
2022-04-22 22:55:17 +0000

    fix(link_sources): don't bail on an invalid version

--- tests/repositories/test_legacy_repository.py
+++ tests/repositories/test_legacy_repository.py
@@ -82,6 +82,37 @@ def test_page_clean_link():
     assert cleaned == "https://legacy.foo.bar/test%20/the%22/cleaning%00"
 
 
+def test_page_invalid_version_link():
+    repo = MockRepository()
+
+    page = repo._get_page("/invalid-version")
+
+    links = list(page.links)
+    assert len(links) == 2
+
+    versions = list(page.versions("poetry"))
+    assert len(versions) == 1
+    assert versions[0].to_string() == "0.1.0"
+
+    invalid_link = None
+
+    for link in links:
+        if link.filename.startswith("poetry-21"):
+            invalid_link = link
+            break
+
+    links_010 = list(page.links_for_version("poetry", versions[0]))
+    assert invalid_link not in links_010
+
+    assert invalid_link
+    assert not page.link_package_data(invalid_link)
+
+    packages = list(page.packages)
+    assert len(packages) == 1
+    assert packages[0].name == "poetry"
+    assert packages[0].version.to_string() == "0.1.0"
+
+
 def test_sdist_format_support():
     repo = MockRepository()
     page = repo._get_page("/relative")
