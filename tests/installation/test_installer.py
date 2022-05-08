c91967408d43bab002149f9e8dc1ad528d25246e
David Hotham <david.hotham@blueyonder.co.uk>
2022-05-08 21:18:22 +0100

    tests: make canonical names forwards compatible

--- tests/installation/test_installer.py
+++ tests/installation/test_installer.py
@@ -1168,6 +1168,14 @@ def test_installer_with_pypi_repository(
     installer.run()
 
     expected = fixture("with-pypi-repository")
+
+    # TODO remove this when https://github.com/python-poetry/poetry-core/pull/328
+    # reaches a published version of poetry-core.
+    extras = locker.written_data["package"][0]["extras"]
+    for key, values in list(extras.items()):
+        extras[key] = [
+            value.replace("zope.interface", "zope-interface") for value in values
+        ]
     assert not DeepDiff(expected, locker.written_data, ignore_order=True)
 
 
