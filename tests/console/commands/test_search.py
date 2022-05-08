d1ba3fe6084cc02fb35151159e27c1cd7ff6831b
David Hotham <david.hotham@blueyonder.co.uk>
2022-05-08 15:44:06 +0100

    backwards-compatible test fix

--- tests/console/commands/test_search.py
+++ tests/console/commands/test_search.py
@@ -96,4 +96,9 @@ sqlalchemy-sqlany (1.0.3)
  SAP Sybase SQL Anywhere dialect for SQLAlchemy
 """
 
-    assert tester.io.fetch_output() == expected
+    # TODO remove this when https://github.com/python-poetry/poetry-core/pull/328
+    # reaches a published version of poetry-core.
+    output = tester.io.fetch_output()
+    output = output.replace("transmogrify.sqlalchemy", "transmogrify-sqlalchemy")
+
+    assert output == expected
