18179661109700e26dedf8d4bd51471175925252
Hadi Alqattan <alqattanhadizaki@gmail.com>
2022-05-17 19:22:21 +0300

    A new unused import detected by Pycln.

--- src/poetry/installation/operations/__init__.py
+++ src/poetry/installation/operations/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from typing import Union
-
 from poetry.installation.operations.install import Install
 from poetry.installation.operations.uninstall import Uninstall
 from poetry.installation.operations.update import Update
