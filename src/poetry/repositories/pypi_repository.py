090cb4f95fcd61b29dc1185e3654c10478a944af
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2022-05-26 23:59:07 +0200

    pypi: do not raise exception on 404

--- src/poetry/repositories/pypi_repository.py
+++ src/poetry/repositories/pypi_repository.py
@@ -243,12 +243,16 @@ class PyPiRepository(HTTPRepository):
 
     def _get(self, endpoint: str) -> dict[str, Any] | None:
         try:
-            json_response = self.session.get(self._base_url + endpoint)
+            json_response = self.session.get(
+                self._base_url + endpoint, raise_for_status=False
+            )
         except requests.exceptions.TooManyRedirects:
             # Cache control redirect loop.
             # We try to remove the cache and try again
             self.session.delete_cache(self._base_url + endpoint)
-            json_response = self.session.get(self._base_url + endpoint)
+            json_response = self.session.get(
+                self._base_url + endpoint, raise_for_status=False
+            )
 
         if json_response.status_code == 404:
             return None
