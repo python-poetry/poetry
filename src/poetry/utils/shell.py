9805314ea6e888098cb02d640db9376d6bc0caf5
Alecto Irene Perez <alecto.perez@voladynamics.com>
2022-06-06 11:13:15 -0600

    Fix shell activation for zsh

--- src/poetry/utils/shell.py
+++ src/poetry/utils/shell.py
@@ -95,8 +95,10 @@ class Shell:
 
         if self._name == "zsh":
             c.setecho(False)
-
-        c.sendline(f"{self._get_source_command()} {shlex.quote(str(activate_path))}")
+            # Under ZSH the source command should be invoked in zsh's bash emulator
+            c.sendline(f"emulate bash -c '. {shlex.quote(str(activate_path))}'")
+        else:
+            c.sendline(f"{self._get_source_command()} {shlex.quote(str(activate_path))}")
 
         def resize(sig: Any, data: Any) -> None:
             terminal = Terminal()
