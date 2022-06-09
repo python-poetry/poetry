583aef80895562ee11f415f2917fc9499d1870cd
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2022-06-10 00:00:17 +0200

    replace entrypoints use with importlib.metadata

--- src/poetry/plugins/plugin_manager.py
+++ src/poetry/plugins/plugin_manager.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
 import logging
-import sys
 
 from typing import TYPE_CHECKING
 
-import entrypoints
-
 from poetry.plugins.application_plugin import ApplicationPlugin
 from poetry.plugins.plugin import Plugin
+from poetry.utils._compat import metadata
 
 
 if TYPE_CHECKING:
@@ -36,16 +34,29 @@ class PluginManager:
 
         plugin_entrypoints = self.get_plugin_entry_points(env=env)
 
-        for entrypoint in plugin_entrypoints:
-            self._load_plugin_entrypoint(entrypoint)
+        for ep in plugin_entrypoints:
+            self._load_plugin_entry_point(ep)
+
+    @staticmethod
+    def _is_plugin_candidate(ep: metadata.EntryPoint, env: Env | None = None) -> bool:
+        """
+        Helper method to check if given entry point is a valid as a plugin candidate.
+        When an environment is specified, the entry point's associated distribution
+        should be installed, and discoverable in the given environment.
+        """
+        return env is None or (
+            ep.dist is not None
+            and env.site_packages.find_distribution(ep.dist.name) is not None
+        )
 
     def get_plugin_entry_points(
         self, env: Env | None = None
-    ) -> list[entrypoints.EntryPoint]:
-        entry_points: list[entrypoints.EntryPoint] = entrypoints.get_group_all(
-            self._group, path=env.sys_path if env else sys.path
-        )
-        return entry_points
+    ) -> list[metadata.EntryPoint]:
+        return [
+            ep
+            for ep in metadata.entry_points(group=self._group)
+            if self._is_plugin_candidate(ep, env)
+        ]
 
     def add_plugin(self, plugin: Plugin) -> None:
         if not isinstance(plugin, (Plugin, ApplicationPlugin)):
@@ -59,10 +70,10 @@ class PluginManager:
         for plugin in self._plugins:
             plugin.activate(*args, **kwargs)
 
-    def _load_plugin_entrypoint(self, entrypoint: entrypoints.EntryPoint) -> None:
-        logger.debug(f"Loading the {entrypoint.name} plugin")
+    def _load_plugin_entry_point(self, ep: metadata.EntryPoint) -> None:
+        logger.debug(f"Loading the {ep.name} plugin")  # type: ignore[attr-defined]
 
-        plugin = entrypoint.load()
+        plugin = ep.load()  # type: ignore[no-untyped-call]
 
         if not issubclass(plugin, (Plugin, ApplicationPlugin)):
             raise ValueError(
