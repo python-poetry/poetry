584aaee5f8da9f950f98d1ef0ebc06fe835e5d96
David Hotham <david.hotham@blueyonder.co.uk>
2022-06-19 23:17:30 +0100

    add timeouts to all requests (#5881)
    * add timeouts to all requests
    * make the requests timeout a constant
    * 15s (matching the default in pip)

--- src/poetry/repositories/http.py
+++ src/poetry/repositories/http.py
@@ -24,6 +24,7 @@ from poetry.repositories.exceptions import PackageNotFound
 from poetry.repositories.exceptions import RepositoryError
 from poetry.repositories.link_sources.html import HTMLPage
 from poetry.utils.authenticator import Authenticator
+from poetry.utils.constants import REQUESTS_TIMEOUT
 from poetry.utils.helpers import download_file
 from poetry.utils.patterns import wheel_file_re
 
@@ -260,7 +261,9 @@ class HTTPRepository(CachedRepository, ABC):
     def _get_response(self, endpoint: str) -> requests.Response | None:
         url = self._url + endpoint
         try:
-            response: requests.Response = self.session.get(url, raise_for_status=False)
+            response: requests.Response = self.session.get(
+                url, raise_for_status=False, timeout=REQUESTS_TIMEOUT
+            )
             if response.status_code in (401, 403):
                 self._log(
                     f"Authorization error accessing {url}",
