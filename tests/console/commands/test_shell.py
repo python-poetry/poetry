ecf4830d76277c0491325c3bf0351718f429eb3d
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2022-08-18 16:31:45 +0200

    test(commands/shell): add tests for `_is_venv_activated`

--- tests/console/commands/test_shell.py
+++ tests/console/commands/test_shell.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pytest
@@ -44,3 +45,40 @@ def test_shell_already_active(tester: CommandTester, mocker: MockerFixture):
     shell_activate.assert_not_called()
     assert tester.io.fetch_output() == expected_output
     assert tester.status_code == 0
+
+
+@pytest.mark.parametrize(
+    ("poetry_active", "real_prefix", "prefix", "expected"),
+    [
+        (None, None, "", False),
+        ("", None, "", False),
+        (" ", None, "", True),
+        ("0", None, "", True),
+        ("1", None, "", True),
+        ("foobar", None, "", True),
+        ("1", "foobar", "foobar", True),
+        (None, "foobar", "foobar", True),
+        (None, "foobar", "foo", True),
+        (None, None, "foobar", True),
+        (None, "foo", "foobar", False),
+        (None, "foo", "foo", False),
+    ],
+)
+def test__is_venv_activated(
+    tester: CommandTester,
+    mocker: MockerFixture,
+    poetry_active: str | None,
+    real_prefix: str | None,
+    prefix: str,
+    expected: bool,
+):
+    mocker.patch.object(tester.command.env, "_path", Path("foobar"))
+    mocker.patch("sys.prefix", prefix)
+
+    if real_prefix is not None:
+        mocker.patch("sys.real_prefix", real_prefix, create=True)
+
+    if poetry_active is not None:
+        os.environ["POETRY_ACTIVE"] = poetry_active
+
+    assert tester.command._is_venv_activated() is expected
