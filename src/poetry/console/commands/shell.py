2ba787c0c1d1b0b539d1e721df3d57c16a64891b
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2022-08-18 14:58:30 +0200

    refactor(commands/shell): move venv check to dedicated function

--- src/poetry/console/commands/shell.py
+++ src/poetry/console/commands/shell.py
@@ -21,10 +21,7 @@ If one doesn't exist yet, it will be created.
         from poetry.utils.shell import Shell
 
         # Check if it's already activated or doesn't exist and won't be created
-        venv_activated = strtobool(environ.get("POETRY_ACTIVE", "0")) or getattr(
-            sys, "real_prefix", sys.prefix
-        ) == str(self.env.path)
-        if venv_activated:
+        if self._is_venv_activated():
             self.line(
                 f"Virtual environment already activated: <info>{self.env.path}</>"
             )
@@ -40,3 +37,8 @@ If one doesn't exist yet, it will be created.
         environ.pop("POETRY_ACTIVE")
 
         return 0
+
+    def _is_venv_activated(self) -> bool:
+        return strtobool(environ.get("POETRY_ACTIVE", "0")) or getattr(
+            sys, "real_prefix", sys.prefix
+        ) == str(self.env.path)
