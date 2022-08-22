f31709feffe94ef07ce4b9b5e6401776dbcaaf7e
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2022-08-22 18:06:05 +0200

    test(compat): conditionally import `Protocol`

--- tests/compat.py
+++ tests/compat.py
@@ -5,10 +5,9 @@ import sys
 
 if sys.version_info < (3, 8):
     import zipp as zipfile  # nopycln: import
+
+    from typing_extensions import Protocol  # nopycln: import
 else:
     import zipfile  # noqa: F401
 
-try:
-    from typing import Protocol  # nopycln: import
-except ImportError:
-    from typing_extensions import Protocol  # noqa: F401
+    from typing import Protocol  # noqa: F401
