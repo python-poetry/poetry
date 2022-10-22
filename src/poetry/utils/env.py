795c56a7ebd931ec9d900dace5c4de4e58273639
David Hotham <david.hotham@blueyonder.co.uk>
2022-10-22 22:15:51 +0100

    remove getattr from SitePackages

--- src/poetry/utils/env.py
+++ src/poetry/utils/env.py
@@ -451,12 +451,6 @@ class SitePackages:
             if value[-1] is True
         ]
 
-    def __getattr__(self, item: str) -> Any:
-        try:
-            return super().__getattribute__(item)
-        except AttributeError:
-            return getattr(self.path, item)
-
 
 class EnvError(Exception):
     pass
