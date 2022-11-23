1e5ec5f95442f3f546d072f7b4bca0abb99818f7
finswimmer <finswimmer77@gmail.com>
2022-11-23 12:51:32 +0100

    chore: apply flake8-class-attributes-order to poetry.publishing

--- src/poetry/publishing/uploader.py
+++ src/poetry/publishing/uploader.py
@@ -84,125 +84,6 @@ class Uploader:
 
         return sorted(wheels + tars)
 
-    def auth(self, username: str | None, password: str | None) -> None:
-        self._username = username
-        self._password = password
-
-    def make_session(self) -> requests.Session:
-        session = requests.session()
-        auth = self.get_auth()
-        if auth is not None:
-            session.auth = auth
-
-        session.headers["User-Agent"] = self.user_agent
-        for scheme in ("http://", "https://"):
-            session.mount(scheme, self.adapter)
-
-        return session
-
-    def get_auth(self) -> tuple[str, str] | None:
-        if self._username is None or self._password is None:
-            return None
-
-        return (self._username, self._password)
-
-    def upload(
-        self,
-        url: str,
-        cert: Path | bool = True,
-        client_cert: Path | None = None,
-        dry_run: bool = False,
-        skip_existing: bool = False,
-    ) -> None:
-        session = self.make_session()
-
-        session.verify = str(cert) if isinstance(cert, Path) else cert
-
-        if client_cert:
-            session.cert = str(client_cert)
-
-        try:
-            self._upload(session, url, dry_run, skip_existing)
-        finally:
-            session.close()
-
-    def post_data(self, file: Path) -> dict[str, Any]:
-        meta = Metadata.from_package(self._package)
-
-        file_type = self._get_type(file)
-
-        if _has_blake2:
-            blake2_256_hash = hashlib.blake2b(digest_size=256 // 8)
-
-        md5_hash = hashlib.md5()
-        sha256_hash = hashlib.sha256()
-        with file.open("rb") as fp:
-            for content in iter(lambda: fp.read(io.DEFAULT_BUFFER_SIZE), b""):
-                md5_hash.update(content)
-                sha256_hash.update(content)
-
-                if _has_blake2:
-                    blake2_256_hash.update(content)
-
-        md5_digest = md5_hash.hexdigest()
-        sha2_digest = sha256_hash.hexdigest()
-        blake2_256_digest: str | None = None
-        if _has_blake2:
-            blake2_256_digest = blake2_256_hash.hexdigest()
-
-        py_version: str | None = None
-        if file_type == "bdist_wheel":
-            wheel_info = wheel_file_re.match(file.name)
-            if wheel_info is not None:
-                py_version = wheel_info.group("pyver")
-
-        data = {
-            # identify release
-            "name": meta.name,
-            "version": meta.version,
-            # file content
-            "filetype": file_type,
-            "pyversion": py_version,
-            # additional meta-data
-            "metadata_version": meta.metadata_version,
-            "summary": meta.summary,
-            "home_page": meta.home_page,
-            "author": meta.author,
-            "author_email": meta.author_email,
-            "maintainer": meta.maintainer,
-            "maintainer_email": meta.maintainer_email,
-            "license": meta.license,
-            "description": meta.description,
-            "keywords": meta.keywords,
-            "platform": meta.platforms,
-            "classifiers": meta.classifiers,
-            "download_url": meta.download_url,
-            "supported_platform": meta.supported_platforms,
-            "comment": None,
-            "md5_digest": md5_digest,
-            "sha256_digest": sha2_digest,
-            "blake2_256_digest": blake2_256_digest,
-            # PEP 314
-            "provides": meta.provides,
-            "requires": meta.requires,
-            "obsoletes": meta.obsoletes,
-            # Metadata 1.2
-            "project_urls": meta.project_urls,
-            "provides_dist": meta.provides_dist,
-            "obsoletes_dist": meta.obsoletes_dist,
-            "requires_dist": meta.requires_dist,
-            "requires_external": meta.requires_external,
-            "requires_python": meta.requires_python,
-        }
-
-        # Metadata 2.1
-        if meta.description_content_type:
-            data["description_content_type"] = meta.description_content_type
-
-        # TODO: Provides extra
-
-        return data
-
     def _upload(
         self,
         session: requests.Session,
@@ -362,3 +243,122 @@ class Uploader:
             # Gitlab Enterprise Edition (https://about.gitlab.com)
             or (status == 400 and "already been taken" in reason_and_text)
         )
+
+    def auth(self, username: str | None, password: str | None) -> None:
+        self._username = username
+        self._password = password
+
+    def make_session(self) -> requests.Session:
+        session = requests.session()
+        auth = self.get_auth()
+        if auth is not None:
+            session.auth = auth
+
+        session.headers["User-Agent"] = self.user_agent
+        for scheme in ("http://", "https://"):
+            session.mount(scheme, self.adapter)
+
+        return session
+
+    def get_auth(self) -> tuple[str, str] | None:
+        if self._username is None or self._password is None:
+            return None
+
+        return (self._username, self._password)
+
+    def upload(
+        self,
+        url: str,
+        cert: Path | bool = True,
+        client_cert: Path | None = None,
+        dry_run: bool = False,
+        skip_existing: bool = False,
+    ) -> None:
+        session = self.make_session()
+
+        session.verify = str(cert) if isinstance(cert, Path) else cert
+
+        if client_cert:
+            session.cert = str(client_cert)
+
+        try:
+            self._upload(session, url, dry_run, skip_existing)
+        finally:
+            session.close()
+
+    def post_data(self, file: Path) -> dict[str, Any]:
+        meta = Metadata.from_package(self._package)
+
+        file_type = self._get_type(file)
+
+        if _has_blake2:
+            blake2_256_hash = hashlib.blake2b(digest_size=256 // 8)
+
+        md5_hash = hashlib.md5()
+        sha256_hash = hashlib.sha256()
+        with file.open("rb") as fp:
+            for content in iter(lambda: fp.read(io.DEFAULT_BUFFER_SIZE), b""):
+                md5_hash.update(content)
+                sha256_hash.update(content)
+
+                if _has_blake2:
+                    blake2_256_hash.update(content)
+
+        md5_digest = md5_hash.hexdigest()
+        sha2_digest = sha256_hash.hexdigest()
+        blake2_256_digest: str | None = None
+        if _has_blake2:
+            blake2_256_digest = blake2_256_hash.hexdigest()
+
+        py_version: str | None = None
+        if file_type == "bdist_wheel":
+            wheel_info = wheel_file_re.match(file.name)
+            if wheel_info is not None:
+                py_version = wheel_info.group("pyver")
+
+        data = {
+            # identify release
+            "name": meta.name,
+            "version": meta.version,
+            # file content
+            "filetype": file_type,
+            "pyversion": py_version,
+            # additional meta-data
+            "metadata_version": meta.metadata_version,
+            "summary": meta.summary,
+            "home_page": meta.home_page,
+            "author": meta.author,
+            "author_email": meta.author_email,
+            "maintainer": meta.maintainer,
+            "maintainer_email": meta.maintainer_email,
+            "license": meta.license,
+            "description": meta.description,
+            "keywords": meta.keywords,
+            "platform": meta.platforms,
+            "classifiers": meta.classifiers,
+            "download_url": meta.download_url,
+            "supported_platform": meta.supported_platforms,
+            "comment": None,
+            "md5_digest": md5_digest,
+            "sha256_digest": sha2_digest,
+            "blake2_256_digest": blake2_256_digest,
+            # PEP 314
+            "provides": meta.provides,
+            "requires": meta.requires,
+            "obsoletes": meta.obsoletes,
+            # Metadata 1.2
+            "project_urls": meta.project_urls,
+            "provides_dist": meta.provides_dist,
+            "obsoletes_dist": meta.obsoletes_dist,
+            "requires_dist": meta.requires_dist,
+            "requires_external": meta.requires_external,
+            "requires_python": meta.requires_python,
+        }
+
+        # Metadata 2.1
+        if meta.description_content_type:
+            data["description_content_type"] = meta.description_content_type
+
+        # TODO: Provides extra
+
+        return data
