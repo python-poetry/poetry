e3f73e34b09a530d5004059532c6bea00d14eadd
SADIK KUZU <sadikkuzu@hotmail.com>
2023-02-25 12:09:31 +0300

    fix(cli): remove config relocation instructions
    No need to have them any more.

--- src/poetry/locations.py
+++ src/poetry/locations.py
@@ -21,31 +21,6 @@ CONFIG_DIR = Path(
     or user_config_path(_APP_NAME, appauthor=False, roaming=True)
 )
 
-# platformdirs 2.0.0 corrected the OSX/macOS config directory from
-# /Users/<user>/Library/Application Support/<appname> to
-# /Users/<user>/Library/Preferences/<appname>.
-#
-# For now we only deprecate use of the old directory.
-if sys.platform == "darwin":
-    _LEGACY_CONFIG_DIR = CONFIG_DIR.parent.parent / "Application Support" / _APP_NAME
-    config_toml = _LEGACY_CONFIG_DIR / "config.toml"
-    auth_toml = _LEGACY_CONFIG_DIR / "auth.toml"
-
-    if any(file.exists() for file in (auth_toml, config_toml)):
-        logger.warning(
-            (
-                (
-                    "Configuration file exists at %s, reusing this"
-                    " directory.\n\nConsider moving TOML configuration files to %s, as"
-                    " support for the legacy directory will be removed in an upcoming"
-                    " release."
-                ),
-            ),
-            _LEGACY_CONFIG_DIR,
-            CONFIG_DIR,
-        )
-        CONFIG_DIR = _LEGACY_CONFIG_DIR
-
 
 def data_dir() -> Path:
     poetry_home = os.getenv("POETRY_HOME")
