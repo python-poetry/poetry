b310a9a44a6c01c1676e85b954607fed09bbe9ce
Bartosz Sokorski <b.sokorski@gmail.com>
2023-05-22 09:48:12 +0200

    Rewrite HTML parsers and drop html5lib (#7924)

--- src/poetry/repositories/link_sources/html.py
+++ src/poetry/repositories/link_sources/html.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import urllib.parse
-import warnings
 
 from collections import defaultdict
 from functools import cached_property
@@ -11,30 +10,30 @@ from typing import TYPE_CHECKING
 from poetry.core.packages.utils.link import Link
 
 from poetry.repositories.link_sources.base import LinkSource
+from poetry.repositories.parsers.html_page_parser import HTMLPageParser
 
 
 if TYPE_CHECKING:
     from poetry.repositories.link_sources.base import LinkCache
 
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    import html5lib
-
-
 class HTMLPage(LinkSource):
     def __init__(self, url: str, content: str) -> None:
         super().__init__(url=url)
 
-        self._parsed = html5lib.parse(content, namespaceHTMLElements=False)
+        parser = HTMLPageParser()
+        parser.feed(content)
+        self._parsed = parser.anchors
+        self._base_url: str | None = parser.base_url
 
     @cached_property
     def _link_cache(self) -> LinkCache:
         links: LinkCache = defaultdict(lambda: defaultdict(list))
-        for anchor in self._parsed.findall(".//a"):
-            if anchor.get("href"):
-                href = anchor.get("href")
-                url = self.clean_link(urllib.parse.urljoin(self._url, href))
+        for anchor in self._parsed:
+            if href := anchor.get("href"):
+                url = self.clean_link(
+                    urllib.parse.urljoin(self._base_url or self._url, href)
+                )
                 pyrequire = anchor.get("data-requires-python")
                 pyrequire = unescape(pyrequire) if pyrequire else None
                 yanked_value = anchor.get("data-yanked")
@@ -42,7 +41,7 @@ class HTMLPage(LinkSource):
                 if yanked_value:
                     yanked = unescape(yanked_value)
                 else:
-                    yanked = "data-yanked" in anchor.attrib
+                    yanked = "data-yanked" in anchor
                 link = Link(url, requires_python=pyrequire, yanked=yanked)
 
                 if link.ext not in self.SUPPORTED_FORMATS:
