f0933cf318498a4309b309f211a58aa74e57f65c
Tzu-Ting <tzingshih@gmail.com>
2023-09-13 22:29:44 +0800

    enhance: test with a minimal repo

--- tests/vcs/git/test_system.py
+++ tests/vcs/git/test_system.py
@@ -3,12 +3,16 @@ from __future__ import annotations
 import re
 import shutil
 import subprocess
+import typing
 
-from pathlib import Path
-
+import dulwich.repo
 import pytest
 
 from poetry.vcs.git.system import SystemGit
+
+
+if typing.TYPE_CHECKING:
+    from pathlib import Path
 
 
 GIT_NOT_INSTALLLED = shutil.which("git") is None
@@ -22,57 +26,54 @@ def get_head_sha(cwd: Path) -> str:
     ).strip()
 
 
-@pytest.fixture(scope="module")
-def current_repo_path() -> Path:
-    return Path(__file__).parents[3]
+class TempRepoFixture(typing.NamedTuple):
+    path: Path
+    repo: dulwich.repo.Repo
+    init_commit: str
+    head_commit: str
 
 
-@pytest.fixture(scope="module")
-def current_sha(current_repo_path: Path) -> str:
-    return get_head_sha(current_repo_path)
+@pytest.fixture()
+def temp_repo(tmp_path: Path) -> TempRepoFixture:
+    """Temporary repository with 2 commits"""
+    repo = dulwich.repo.Repo.init(str(tmp_path))
 
+    # init commit
+    (tmp_path / "foo").write_text("foo")
+    repo.stage(["foo"])
 
-@pytest.fixture
-def tmp_repo(current_repo_path: Path, tmp_path: Path, current_sha: str) -> Path:
-    """Temporary repository + 1 redundant commit"""
-    # create repo
-    target_dir = tmp_path / "poetry-test"
-    SystemGit.clone(current_repo_path.as_uri(), target_dir)
-
-    # configure author
-    subprocess.check_output(["git", "config", "user.name", "User"], cwd=target_dir)
-    subprocess.check_output(
-        ["git", "config", "user.email", "user@example.com"], cwd=target_dir
+    init_commit = repo.do_commit(
+        committer=b"User <user@example.com>",
+        author=b"User <user@example.com>",
+        message=b"init",
+        no_verify=True,
     )
 
-    # add redundant commit
-    stdout = subprocess.check_output(
-        [
-            "git",
-            "commit",
-            "--allow-empty",
-            "--message=test commit",
-            "--no-gpg-sign",  # avoid user interaction in case signing is turned on
-        ],
-        cwd=target_dir,
-        text=True,
+    # extra commit
+    (tmp_path / "foo").write_text("bar")
+    repo.stage(["foo"])
+
+    head_commit = repo.do_commit(
+        committer=b"User <user@example.com>",
+        author=b"User <user@example.com>",
+        message=b"extra",
+        no_verify=True,
     )
 
-    # ensure head commit changed
-    assert re.match(r"^\[.+\] test commit", stdout)
-    assert get_head_sha(target_dir) != current_sha
-
-    return target_dir
+    return TempRepoFixture(
+        path=tmp_path,
+        repo=repo,
+        init_commit=init_commit.decode(),
+        head_commit=head_commit.decode(),
+    )
 
 
 @pytest.mark.skipif(GIT_NOT_INSTALLLED, reason="These tests requires git cli")
 class TestSystemGit:
-    def test_clone_success(self, current_repo_path: Path, tmp_path: Path) -> None:
-        target_dir = tmp_path / "poetry-test"
-
-        stdout = SystemGit.clone(current_repo_path.as_uri(), target_dir)
-
-        assert re.search(r"Cloning into '.+[\\/]poetry-test'...", stdout)
+    def test_clone_success(self, tmp_path: Path, temp_repo: TempRepoFixture) -> None:
+        target_dir = tmp_path / "test-repo"
+        stdout = SystemGit.clone(temp_repo.path.as_uri(), target_dir)
+        assert re.search(r"Cloning into '.+[\\/]test-repo'...", stdout)
         assert (target_dir / ".git").is_dir()
 
     def test_clone_invalid_parameter(self, tmp_path: Path) -> None:
@@ -81,15 +82,16 @@ class TestSystemGit:
         ):
             SystemGit.clone("--upload-pack=touch ./HELL", tmp_path)
 
-    def test_checkout_1(self, tmp_repo: Path, current_sha: str) -> None:
+    def test_checkout_1(self, temp_repo: TempRepoFixture) -> None:
         # case 1 - with 'target' arg
-        SystemGit.checkout(current_sha[:12], tmp_repo)
-        assert get_head_sha(tmp_repo) == current_sha
+        SystemGit.checkout(temp_repo.init_commit[:12], temp_repo.path)
+        assert get_head_sha(temp_repo.path) == temp_repo.init_commit
 
     def test_checkout_2(
-        self, monkeypatch: pytest.MonkeyPatch, tmp_repo: Path, current_sha: str
+        self, monkeypatch: pytest.MonkeyPatch, temp_repo: TempRepoFixture
     ) -> None:
         # case 2 - without 'target' arg
-        monkeypatch.chdir(tmp_repo)
-        SystemGit.checkout(current_sha[:12])
-        assert get_head_sha(tmp_repo) == current_sha
+        monkeypatch.chdir(temp_repo.path)
+
+        SystemGit.checkout(temp_repo.init_commit[:12])
+        assert get_head_sha(temp_repo.path) == temp_repo.init_commit
