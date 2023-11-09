85577dab8bd8bf082eb499de1b809c2bbea0b3e7
ArchGuy <wubo2929470442@gmail.com>
2023-11-09 22:49:24 +0800

    Refactor virtualenv bin dir handling for Windows

--- src/poetry/utils/shell.py
+++ src/poetry/utils/shell.py
@@ -70,7 +70,16 @@ class Shell:
 
     def activate(self, env: VirtualEnv) -> int | None:
         activate_script = self._get_activate_script()
-        bin_dir = "Scripts" if WINDOWS else "bin"
+        if WINDOWS:
+            bin_dir = "Scripts"
+            if os.path.exists(str(env.path / bin_dir)) {
+                return bin_dir
+            }
+            # Python innstalled via msys2 on Windows might produce a POSIX-like venv
+            # See https://github.com/python-poetry/poetry/issues/8638
+            bin_dir = "bin"
+        else:
+            bin_dir = "bin"
         activate_path = env.path / bin_dir / activate_script
 
         # mypy requires using sys.platform instead of WINDOWS constant
