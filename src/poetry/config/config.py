c1abbb9f44d54b937942a1ac8edd189a90774984
David Hotham <david.hotham@blueyonder.co.uk>
2023-11-11 13:42:01 +0000

    Complete the removal of poetry-export

--- src/poetry/config/config.py
+++ src/poetry/config/config.py
@@ -136,9 +136,6 @@ class Config:
         "solver": {
             "lazy-wheel": True,
         },
-        "warnings": {
-            "export": True,
-        },
         "keyring": {
             "enabled": True,
         },
@@ -308,7 +305,6 @@ class Config:
             "experimental.system-git-client",
             "installer.parallel",
             "solver.lazy-wheel",
-            "warnings.export",
             "keyring.enabled",
         }:
             return boolean_normalizer
