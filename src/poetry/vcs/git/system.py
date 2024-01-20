9756b57995bd5cf5e0b7f89bd08a7c71ae6c7d98
Kateryna Khramova <36553924+kkhramova@users.noreply.github.com>
2024-01-20 05:38:52 -0500

    Fix git clone test on non-English locale (#8843)

--- src/poetry/vcs/git/system.py
+++ src/poetry/vcs/git/system.py
@@ -15,18 +15,18 @@ if TYPE_CHECKING:
 
 class SystemGit:
     @classmethod
-    def clone(cls, repository: str, dest: Path) -> str:
+    def clone(cls, repository: str, dest: Path) -> None:
         cls._check_parameter(repository)
 
-        return cls.run("clone", "--recurse-submodules", "--", repository, str(dest))
+        cls.run("clone", "--recurse-submodules", "--", repository, str(dest))
 
     @classmethod
-    def checkout(cls, rev: str, target: Path | None = None) -> str:
+    def checkout(cls, rev: str, target: Path | None = None) -> None:
         cls._check_parameter(rev)
-        return cls.run("checkout", rev, folder=target)
+        cls.run("checkout", rev, folder=target)
 
     @staticmethod
-    def run(*args: Any, **kwargs: Any) -> str:
+    def run(*args: Any, **kwargs: Any) -> None:
         folder = kwargs.pop("folder", None)
         if folder:
             args = (
@@ -40,12 +40,13 @@ class SystemGit:
         git_command = find_git_command()
         env = os.environ.copy()
         env["GIT_TERMINAL_PROMPT"] = "0"
-        return subprocess.check_output(
+        subprocess.check_call(
             git_command + list(args),
-            stderr=subprocess.STDOUT,
+            stderr=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
             env=env,
             text=True,
-        ).strip()
+        )
 
     @staticmethod
     def _check_parameter(parameter: str) -> None:
