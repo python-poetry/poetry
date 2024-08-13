df4c200e0f5dd149278c2fc1da627abaf24f43f4
j.schnaecker <jon.schnaecker@gmail.com>
2024-08-13 11:42:14 +0200

    adds tests for isolated_environment

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from tests.helpers import flatten_dict
+import os
+
+from tests.helpers import flatten_dict, isolated_environment
 
 
 def test_flatten_dict() -> None:
@@ -21,3 +23,23 @@ def test_flatten_dict() -> None:
     }
 
     assert flattened_dict == flatten_dict(orig_dict, delimiter=":")
+
+
+def test_isolated_environment_restores_original_environ() -> None:
+    original_environ = dict(os.environ)
+    with isolated_environment():
+        os.environ["TEST_VAR"] = "test"
+    assert os.environ == original_environ
+
+
+def test_isolated_environment_clears_environ() -> None:
+    os.environ["TEST_VAR"] = "test"
+    with isolated_environment(clear=True):
+        assert "TEST_VAR" not in os.environ
+    assert "TEST_VAR" in os.environ
+
+
+def test_isolated_environment_updates_environ() -> None:
+    with isolated_environment(environ={"NEW_VAR": "new_value"}):
+        assert os.environ["NEW_VAR"] == "new_value"
+    assert "NEW_VAR" not in os.environ
