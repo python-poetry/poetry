4d81789859a9a18fc4a760dd39edb4e25c4ae5b0
finswimmer <finswimmer77@gmail.com>
2024-11-04 06:56:34 +0100

    feat(config): add ConfigSourceMigration
    Rename or delete keys from ConfigSource. Migrate existing values including unset them.

--- src/poetry/config/config_source.py
+++ src/poetry/config/config_source.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
+import dataclasses
+
 from abc import ABC
 from abc import abstractmethod
 from typing import Any
+
+
+UNSET = object()
 
 
 class PropertyNotFoundError(ValueError):
@@ -18,6 +23,28 @@ class ConfigSource(ABC):
 
     @abstractmethod
     def remove_property(self, key: str) -> None: ...
+
+
+@dataclasses.dataclass
+class ConfigSourceMigration:
+    old_key: str
+    new_key: str | None
+    value_migration: dict[Any, Any] = dataclasses.field(default_factory=dict)
+
+    def apply(self, config_source: ConfigSource) -> None:
+        try:
+            old_value = config_source.get_property(self.old_key)
+        except PropertyNotFoundError:
+            return
+
+        new_value = (
+            self.value_migration[old_value] if self.value_migration else old_value
+        )
+
+        config_source.remove_property(self.old_key)
+
+        if self.new_key is not None and new_value is not UNSET:
+            config_source.add_property(self.new_key, new_value)
 
 
 def drop_empty_config_category(
--- tests/config/test_config_source.py
+++ tests/config/test_config_source.py
@@ -4,7 +4,10 @@ from typing import Any
 
 import pytest
 
+from poetry.config.config_source import UNSET
+from poetry.config.config_source import ConfigSourceMigration
 from poetry.config.config_source import drop_empty_config_category
+from poetry.config.dict_config_source import DictConfigSource
 
 
 @pytest.mark.parametrize(
@@ -51,3 +54,105 @@ def test_drop_empty_config_category(
         )
         == expected
     )
+
+
+def test_config_source_migration_rename_key() -> None:
+    config_data = {
+        "virtualenvs": {
+            "prefer-active-python": True,
+        },
+        "system-git-client": True,
+    }
+
+    config_source = DictConfigSource()
+    config_source._config = config_data
+
+    migration = ConfigSourceMigration(
+        old_key="virtualenvs.prefer-active-python",
+        new_key="virtualenvs.use-poetry-python",
+    )
+
+    migration.apply(config_source)
+
+    config_source._config = {
+        "virtualenvs": {
+            "use-poetry-python": True,
+        },
+        "system-git-client": True,
+    }
+
+
+def test_config_source_migration_remove_key() -> None:
+    config_data = {
+        "virtualenvs": {
+            "prefer-active-python": True,
+        },
+        "system-git-client": True,
+    }
+
+    config_source = DictConfigSource()
+    config_source._config = config_data
+
+    migration = ConfigSourceMigration(
+        old_key="virtualenvs.prefer-active-python",
+        new_key=None,
+    )
+
+    migration.apply(config_source)
+
+    config_source._config = {
+        "virtualenvs": {},
+        "system-git-client": True,
+    }
+
+
+def test_config_source_migration_unset_value() -> None:
+    config_data = {
+        "virtualenvs": {
+            "prefer-active-python": True,
+        },
+        "system-git-client": True,
+    }
+
+    config_source = DictConfigSource()
+    config_source._config = config_data
+
+    migration = ConfigSourceMigration(
+        old_key="virtualenvs.prefer-active-python",
+        new_key="virtualenvs.use-poetry-python",
+        value_migration={True: UNSET, False: True},
+    )
+
+    migration.apply(config_source)
+
+    config_source._config = {
+        "virtualenvs": {},
+        "system-git-client": True,
+    }
+
+
+def test_config_source_migration_complex_migration() -> None:
+    config_data = {
+        "virtualenvs": {
+            "prefer-active-python": True,
+        },
+        "system-git-client": True,
+    }
+
+    config_source = DictConfigSource()
+    config_source._config = config_data
+
+    migration = ConfigSourceMigration(
+        old_key="virtualenvs.prefer-active-python",
+        new_key="virtualenvs.use-poetry-python",
+        value_migration={True: None, False: True},
+    )
+
+    migration.apply(config_source)
+
+    config_source._config = {
+        "virtualenvs": {
+            "use-poetry-python": None,
+        },
+        "system-git-client": True,
+    }
