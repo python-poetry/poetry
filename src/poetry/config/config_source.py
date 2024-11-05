677ba8126cfe9a36d6226226f17dd491eff7e32b
finswimmer <finswimmer77@gmail.com>
2024-11-05 07:23:13 +0100

    feat(config): provide method to remove empty config category
    For now only applied to FileConfigSource on remove_property

--- src/poetry/config/config_source.py
+++ src/poetry/config/config_source.py
@@ -18,3 +18,22 @@ class ConfigSource(ABC):
 
     @abstractmethod
     def remove_property(self, key: str) -> None: ...
+
+
+def drop_empty_config_category(
+    keys: list[str], config: dict[Any, Any]
+) -> dict[Any, Any]:
+    config_ = {}
+
+    for key, value in config.items():
+        if not keys or key != keys[0]:
+            config_[key] = value
+            continue
+        if keys and key == keys[0]:
+            if isinstance(value, dict):
+                value = drop_empty_config_category(keys[1:], value)
+
+            if value != {}:
+                config_[key] = value
+
+    return config_
