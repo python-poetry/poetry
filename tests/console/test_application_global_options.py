f37ba9420d470f43122bcef23bffc663d2c6d842
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2025-01-12 00:07:29 +0100

    chore(test): refactor app context tests

--- tests/console/test_application_global_options.py
+++ tests/console/test_application_global_options.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import textwrap
+
 from pathlib import Path
 from typing import TYPE_CHECKING
 
@@ -8,9 +10,14 @@ import pytest
 from cleo.testers.application_tester import ApplicationTester
 
 from poetry.console.application import Application
+from poetry.console.commands.version import VersionCommand
+from tests.helpers import switch_working_directory
 
 
 if TYPE_CHECKING:
+    from pytest import TempPathFactory
+    from pytest_mock import MockerFixture
+
     from tests.types import FixtureCopier
 
 
@@ -25,6 +32,21 @@ def project_source_directory(fixture_copier: FixtureCopier) -> Path:
 @pytest.fixture
 def tester() -> ApplicationTester:
     return ApplicationTester(Application())
+
+
+@pytest.fixture
+def with_mocked_version_command(mocker: MockerFixture) -> None:
+    orig_version_command = VersionCommand.handle
+
+    def mock_handle(command: VersionCommand) -> int:
+        exit_code = orig_version_command(command)
+
+        command.io.write_line(f"ProjectPath: {command.poetry.pyproject_path.parent}")
+        command.io.write_line(f"WorkingDirectory: {Path.cwd()}")
+
+        return exit_code
+
+    mocker.patch("poetry.console.commands.version.VersionCommand.handle", mock_handle)
 
 
 def test_application_global_option_ensure_error_when_context_invalid(
@@ -68,3 +90,62 @@ def test_application_global_option_position_does_not_matter(
 
     assert "certifi" in stdout
     assert len(stdout.splitlines()) == 8
+
+
+@pytest.mark.parametrize("parameter", ["project", "directory"])
+def test_application_with_context_parameters(
+    parameter: str,
+    tester: ApplicationTester,
+    project_source_directory: Path,
+    with_mocked_version_command: None,
+) -> None:
+    # ensure pre-conditions are met
+    assert project_source_directory != Path.cwd()
+
+    is_directory_param = parameter == "directory"
+
+    tester.execute(f"--{parameter} {project_source_directory} version")
+    assert tester.io.fetch_error() == ""
+    assert tester.status_code == 0
+
+    output = tester.io.fetch_output()
+    assert output == textwrap.dedent(f"""\
+    foobar 0.1.0
+    ProjectPath: {project_source_directory}
+    WorkingDirectory: {project_source_directory if is_directory_param else Path.cwd()}
+    """)
+
+
+def test_application_with_relative_project_parameter(
+    tester: ApplicationTester,
+    project_source_directory: Path,
+    with_mocked_version_command: None,
+    tmp_path_factory: TempPathFactory,
+) -> None:
+    # ensure pre-conditions are met
+    cwd = Path.cwd()
+    assert project_source_directory.is_relative_to(cwd)
+
+    # construct relative path
+    relative_source_directory = project_source_directory.relative_to(cwd)
+    assert relative_source_directory.as_posix() != project_source_directory.as_posix()
+    assert not relative_source_directory.is_absolute()
+
+    # we expect application run to be executed within current cwd but project to be a subdirectory
+    args = f"--directory '{cwd}' --project {relative_source_directory} version"
+
+    # we switch cwd to a new temporary directory unrelated to the project directory
+    new_working_dir = tmp_path_factory.mktemp("unrelated-working-directory")
+    with switch_working_directory(new_working_dir):
+        assert Path.cwd() == new_working_dir
+
+        tester.execute(args)
+        assert tester.io.fetch_error() == ""
+        assert tester.status_code == 0
+
+        output = tester.io.fetch_output()
+        assert output == textwrap.dedent(f"""\
+        foobar 0.1.0
+        ProjectPath: {project_source_directory}
+        WorkingDirectory: {cwd}
+        """)
