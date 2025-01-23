python-poetry/poetry

commit ff3f8bd6337ade555d4a24c76e2e68116f30f310

fix(application): check context options preflight
This change ensures that context options like --project and --directory
are validated prior to executing the application to prevent runtime
exceptions.

src/poetry/console/application.py
@@ -20,6 +20,7 @@ from poetry.__version__ import __version__
 from poetry.console.command_loader import CommandLoader
 from poetry.console.commands.command import Command
 from poetry.utils.helpers import directory
+from poetry.utils.helpers import ensure_path
 
 
 if TYPE_CHECKING:
@@ -104,7 +105,7 @@ class Application(BaseApplication):
         self._disable_cache = False
         self._plugins_loaded = False
         self._working_directory = Path.cwd()
-        self._project_directory = self._working_directory
+        self._project_directory: Path | None = None
 
         dispatcher = EventDispatcher()
         dispatcher.add_listener(COMMAND, self.register_command_loggers)
@@ -159,6 +160,10 @@ class Application(BaseApplication):
         return definition
 
     @property
+    def project_directory(self) -> Path:
+        return self._project_directory or self._working_directory
+
+    @property
     def poetry(self) -> Poetry:
         from poetry.factory import Factory
 
@@ -166,7 +171,7 @@ class Application(BaseApplication):
             return self._poetry
 
         self._poetry = Factory().create_poetry(
-            cwd=self._project_directory,
+            cwd=self.project_directory,
             io=self._io,
             disable_plugins=self._disable_plugins,
             disable_cache=self._disable_cache,
@@ -214,6 +219,12 @@ class Application(BaseApplication):
         return io
 
     def _run(self, io: IO) -> int:
+        # we do this here and not inside the _configure_io implementation in order
+        # to ensure the users are not exposed to a stack trace for providing invalid values to
+        # the options --directory or --project, configuring the options here allow cleo to trap and
+        # display the error cleanly unless the user uses verbose or debug
+        self._configure_custom_application_options(io)
+
         self._load_plugins(io)
 
         with directory(self._working_directory):
@@ -247,17 +258,24 @@ class Application(BaseApplication):
         self._disable_cache = self._option_get_value(
             io, "no-cache", self._disable_cache
         )
-        self._working_directory = self._project_directory = Path(
-            self._option_get_value(io, "directory", Path.cwd())
+
+        # we use ensure_path for the directories to make sure these are valid paths
+        # this will raise an exception if the path is invalid
+        self._working_directory = ensure_path(
+            self._option_get_value(io, "directory", Path.cwd()), is_directory=True
         )
 
-        self._project_directory = Path(
-            self._option_get_value(io, "project", self._working_directory)
-        )
-
-        if self._project_directory != self._working_directory:
-            with directory(self._working_directory):
-                self._project_directory = self._project_directory.absolute()
+        self._project_directory = self._option_get_value(io, "project", None)
+        if self._project_directory is not None:
+            self._project_directory = Path(self._project_directory)
+            self._project_directory = ensure_path(
+                self._project_directory
+                if self._project_directory.is_absolute()
+                else self._working_directory.joinpath(self._project_directory).resolve(
+                    strict=False
+                ),
+                is_directory=True,
+            )
 
     def _configure_io(self, io: IO) -> None:
         # We need to check if the command being run
@@ -294,8 +312,6 @@ class Application(BaseApplication):
             io.set_input(run_input)
 
         super()._configure_io(io)
-
-        self._configure_custom_application_options(io)
 
     def register_command_loggers(
         self, event: Event, event_name: str, _: EventDispatcher
@@ -418,7 +434,7 @@ class Application(BaseApplication):
             from poetry.plugins.application_plugin import ApplicationPlugin
             from poetry.plugins.plugin_manager import PluginManager
 
-            PluginManager.add_project_plugin_path(self._project_directory)
+            PluginManager.add_project_plugin_path(self.project_directory)
             manager = PluginManager(ApplicationPlugin.group)
             manager.load_plugins()
             manager.activate(self)

src/poetry/utils/helpers.py
@@ -258,6 +258,18 @@ def paths_csv(paths: list[Path]) -> str:
     return ", ".join(f'"{c!s}"' for c in paths)
 
 
+def ensure_path(path: str | Path, is_directory: bool = False) -> Path:
+    if isinstance(path, str):
+        path = Path(path)
+
+    if path.exists() and path.is_dir() == is_directory:
+        return path
+
+    raise ValueError(
+        f"Specified path '{path}' is not a valid {'directory' if is_directory else 'file'}."
+    )
+
+
 def is_dir_writable(path: Path, create: bool = False) -> bool:
     try:
         if not path.exists():

tests/console/test_application_global_options.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import re
 import textwrap
 
 from pathlib import Path
@@ -90,6 +91,33 @@ def test_application_global_option_position_does_not_matter(
 
     assert "certifi" in stdout
     assert len(stdout.splitlines()) == 8
+
+
+@pytest.mark.parametrize("parameter", ["-C", "--directory", "-P", "--project"])
+@pytest.mark.parametrize(
+    "invalid_source_directory",
+    [
+        "/invalid/path",  # non-existent path
+        __file__,  # not a directory
+    ],
+)
+def test_application_global_option_context_is_validated(
+    parameter: str,
+    tester: ApplicationTester,
+    invalid_source_directory: str,
+) -> None:
+    option = f"{parameter} '{invalid_source_directory}'"
+    tester.execute(f"show {option}")
+    assert tester.status_code != 0
+
+    stdout = tester.io.fetch_output()
+    assert stdout == ""
+
+    stderr = tester.io.fetch_error()
+    assert re.match(
+        r"\nSpecified path '(.+)?' is not a valid directory.\n",
+        stderr,
+    )
 
 
 @pytest.mark.parametrize("parameter", ["project", "directory"])

tests/utils/test_helpers.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import base64
 import re
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 from typing import Any
 
@@ -14,13 +15,12 @@ from requests.exceptions import ChunkedEncodingError
 from poetry.utils.helpers import Downloader
 from poetry.utils.helpers import HTTPRangeRequestSupportedError
 from poetry.utils.helpers import download_file
+from poetry.utils.helpers import ensure_path
 from poetry.utils.helpers import get_file_hash
 from poetry.utils.helpers import get_highest_priority_hash_type
 
 
 if TYPE_CHECKING:
-    from pathlib import Path
-
     from httpretty import httpretty
     from httpretty.core import HTTPrettyRequest
 
@@ -299,3 +299,47 @@ def test_downloader_uses_authenticator_by_default(
     request = http.last_request()
     basic_auth = base64.b64encode(b"bar:baz").decode()
     assert request.headers["Authorization"] == f"Basic {basic_auth}"
+
+
+def test_ensure_path_converts_string(tmp_path: Path) -> None:
+    assert tmp_path.exists()
+    assert ensure_path(path=tmp_path.as_posix(), is_directory=True) == tmp_path
+
+
+def test_ensure_path_does_not_convert_path(tmp_path: Path) -> None:
+    assert tmp_path.exists()
+    assert Path(tmp_path.as_posix()) is not tmp_path
+
+    result = ensure_path(path=tmp_path, is_directory=True)
+
+    assert result == tmp_path
+    assert result is tmp_path
+
+
+def test_ensure_path_is_directory_parameter(tmp_path: Path) -> None:
+    with pytest.raises(ValueError):
+        ensure_path(path=tmp_path, is_directory=False)
+
+    assert ensure_path(path=tmp_path, is_directory=True) is tmp_path
+
+
+def test_ensure_path_file(tmp_path: Path) -> None:
+    path = tmp_path.joinpath("some_file.txt")
+    assert not path.exists()
+
+    with pytest.raises(ValueError):
+        ensure_path(path=path, is_directory=False)
+
+    path.write_text("foobar")
+    assert ensure_path(path=path, is_directory=False) is path
+
+
+def test_ensure_path_directory(tmp_path: Path) -> None:
+    path = tmp_path.joinpath("foobar")
+    assert not path.exists()
+
+    with pytest.raises(ValueError):
+        ensure_path(path=path, is_directory=True)
+
+    path.mkdir()
+    assert ensure_path(path=path, is_directory=True) is path

commit 5af439f38b0d11b6a0d1202c5995b1eb653d1220

fix(app): improve global opt and run cmd parsing
This change fixes an issue introduced in #10021 that led to options
provided in the form `-<shortcut><value>` to be incorrectly handled.

Further, it also simplifies and fixes run command processing.

Resolves: #10051

src/poetry/console/application.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
+import argparse
 import logging
-import re
 
 from contextlib import suppress
 from importlib import import_module
@@ -16,6 +16,7 @@ from cleo.events.event_dispatcher import EventDispatcher
 from cleo.exceptions import CleoCommandNotFoundError
 from cleo.exceptions import CleoError
 from cleo.formatters.style import Style
+from cleo.io.inputs.argv_input import ArgvInput
 
 from poetry.__version__ import __version__
 from poetry.console.command_loader import CommandLoader
@@ -26,10 +27,8 @@ from poetry.utils.helpers import ensure_path
 
 if TYPE_CHECKING:
     from collections.abc import Callable
-    from typing import Any
 
     from cleo.events.event import Event
-    from cleo.io.inputs.argv_input import ArgvInput
     from cleo.io.inputs.definition import Definition
     from cleo.io.inputs.input import Input
     from cleo.io.io import IO
@@ -243,7 +242,7 @@ class Application(BaseApplication):
         # to ensure the users are not exposed to a stack trace for providing invalid values to
         # the options --directory or --project, configuring the options here allow cleo to trap and
         # display the error cleanly unless the user uses verbose or debug
-        self._configure_custom_application_options(io)
+        self._configure_global_options(io)
 
         self._load_plugins(io)
 
@@ -265,40 +264,29 @@ class Application(BaseApplication):
 
         return exit_code
 
-    def _option_get_value(self, io: IO, name: str, default: Any) -> Any:
-        option = self.definition.option(name)
+    def _configure_global_options(self, io: IO) -> None:
+        """
+        Configures global options for the application by setting up the relevant
+        directories, disabling plugins or cache, and managing the working and
+        project directories. This method ensures that all directories are valid
+        paths and handles the resolution of the project directory relative to the
+        working directory if necessary.
 
-        if option is None:
-            return default
+        :param io: The IO instance whose input and options are being read.
+        :return: Nothing.
+        """
+        self._sort_global_options(io)
 
-        values = [f"--{option.name}"]
-
-        if option.shortcut:
-            values.append(f"-{option.shortcut}")
-
-        if not io.input.has_parameter_option(values):
-            return default
-
-        if option.is_flag():
-            return True
-
-        return io.input.parameter_option(values=values, default=default)
-
-    def _configure_custom_application_options(self, io: IO) -> None:
-        self._disable_plugins = self._option_get_value(
-            io, "no-plugins", self._disable_plugins
-        )
-        self._disable_cache = self._option_get_value(
-            io, "no-cache", self._disable_cache
-        )
+        self._disable_plugins = io.input.option("no-plugins")
+        self._disable_cache = io.input.option("no-cache")
 
         # we use ensure_path for the directories to make sure these are valid paths
         # this will raise an exception if the path is invalid
         self._working_directory = ensure_path(
-            self._option_get_value(io, "directory", Path.cwd()), is_directory=True
+            io.input.option("directory") or Path.cwd(), is_directory=True
         )
 
-        self._project_directory = self._option_get_value(io, "project", None)
+        self._project_directory = io.input.option("project")
         if self._project_directory is not None:
             self._project_directory = Path(self._project_directory)
             self._project_directory = ensure_path(
@@ -310,40 +298,151 @@ class Application(BaseApplication):
                 is_directory=True,
             )
 
-    def _configure_io(self, io: IO) -> None:
-        # We need to check if the command being run
-        # is the "run" command.
-        definition = self.definition
+    def _sort_global_options(self, io: IO) -> None:
+        """
+        Sorts global options of the provided IO instance according to the
+        definition of the available options, reordering and parsing arguments
+        to ensure consistency in input handling.
+
+        The function interprets the options and their corresponding values
+        using an argument parser, constructs a sorted list of tokens, and
+        recreates the input with the rearranged sequence while maintaining
+        compatibility with the initially provided input stream.
+
+        If using in conjunction with `_configure_run_command`, it is recommended that
+        it be called first in order to correctly handling cases like
+        `poetry run -V python -V`.
+
+        :param io: The IO instance whose input and options are being processed
+                   and reordered.
+        :return: Nothing.
+        """
+        original_input = cast(ArgvInput, io.input)
+        tokens: list[str] = original_input._tokens
+
+        parser = argparse.ArgumentParser(add_help=False)
+
+        for option in self.definition.options:
+            parser.add_argument(
+                f"--{option.name}",
+                *([f"-{option.shortcut}"] if option.shortcut else []),
+                action="store_true" if option.is_flag() else "store",
+            )
+
+        args, remaining_args = parser.parse_known_args(tokens)
+
+        tokens = []
+        for option in self.definition.options:
+            key = option.name.replace("-", "_")
+            value = getattr(args, key, None)
+
+            if value is not None:
+                if value:  # is truthy
+                    tokens.append(f"--{option.name}")
+
+                if option.accepts_value():
+                    tokens.append(str(value))
+
+        sorted_input = ArgvInput([self._name or "", *tokens, *remaining_args])
+        sorted_input.set_stream(original_input.stream)
+
         with suppress(CleoError):
-            io.input.bind(definition)
+            sorted_input.bind(self.definition)
 
-        name = io.input.first_argument
-        if name == "run":
-            from poetry.console.io.inputs.run_argv_input import RunArgvInput
+        io.set_input(sorted_input)
 
-            input = cast("ArgvInput", io.input)
-            run_input = RunArgvInput([self._name or "", *input._tokens])
-            # For the run command reset the definition
-            # with only the set options (i.e. the options given before the command)
-            for option_name, value in input.options.items():
-                if value:
-                    option = definition.option(option_name)
-                    run_input.add_parameter_option("--" + option.name)
-                    if option.shortcut:
-                        shortcuts = re.split(r"\|-?", option.shortcut.lstrip("-"))
-                        shortcuts = [s for s in shortcuts if s]
-                        for shortcut in shortcuts:
-                            run_input.add_parameter_option("-" + shortcut.lstrip("-"))
+    def _configure_run_command(self, io: IO) -> None:
+        """
+        Configures the input for the "run" command to properly handle cases where the user
+        executes commands such as "poetry run -- <subcommand>". This involves reorganizing
+        input tokens to ensure correct parsing and execution of the run command.
+        """
+        with suppress(CleoError):
+            io.input.bind(self.definition)
+
+        command_name = io.input.first_argument
+
+        if command_name == "run":
+            original_input = cast(ArgvInput, io.input)
+            tokens: list[str] = original_input._tokens
+
+            if "--" in tokens:
+                # this means the user has done the right thing and used "poetry run -- echo hello"
+                # in this case there is not much we need to do, we can skip the rest
+                return
+
+            # find the correct command index, in some cases this might not be first occurrence
+            # eg: poetry -C run run echo
+            command_index = tokens.index(command_name)
+
+            while command_index < (len(tokens) - 1):
+                try:
+                    # try parsing the tokens so far
+                    _ = ArgvInput(
+                        [self._name or "", *tokens[: command_index + 1]],
+                        definition=self.definition,
+                    )
+                    break
+                except CleoError:
+                    # parsing failed, try finding the next "run" token
+                    try:
+                        command_index += (
+                            tokens[command_index + 1 :].index(command_name) + 1
+                        )
+                    except ValueError:
+                        command_index = len(tokens)
+            else:
+                # looks like we reached the end of the road, let clea deal with it
+                return
+
+            # fetch tokens after the "run" command
+            tokens_without_command = tokens[command_index + 1 :]
+
+            # we create a new input for parsing the subcommand pretending
+            # it is poetry command
+            without_command = ArgvInput(
+                [self._name or "", *tokens_without_command], None
+            )
 
             with suppress(CleoError):
-                run_input.bind(definition)
+                # we want to bind the definition here so that cleo knows what should be
+                # parsed, and how
+                without_command.bind(self.definition)
 
-            for option_name, value in input.options.items():
-                if value:
-                    run_input.set_option(option_name, value)
+            # the first argument here is the subcommand
+            subcommand = without_command.first_argument
+            subcommand_index = (
+                (tokens_without_command.index(subcommand) if subcommand else 0)
+                + command_index
+                + 1
+            )
 
+            # recreate the original input reordering in the following order
+            #   - all tokens before "run" command
+            #   - all tokens after "run" command but before the subcommand
+            #   - the "run" command token
+            #   - the "--" token to normalise the form
+            #   - all remaining tokens starting with the subcommand
+            run_input = ArgvInput(
+                [
+                    self._name or "",
+                    *tokens[:command_index],
+                    *tokens[command_index + 1 : subcommand_index],
+                    command_name,
+                    "--",
+                    *tokens[subcommand_index:],
+                ]
+            )
+            run_input.set_stream(original_input.stream)
+
+            with suppress(CleoError):
+                run_input.bind(self.definition)
+
+            # reset the input to our constructed form
             io.set_input(run_input)
 
+    def _configure_io(self, io: IO) -> None:
+        self._configure_run_command(io)
         super()._configure_io(io)
 
     def register_command_loggers(

src/poetry/console/io/inputs/run_argv_input.py
@@ -1,83 +1,0 @@
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
-
-from cleo.io.inputs.argv_input import ArgvInput
-
-
-if TYPE_CHECKING:
-    from cleo.io.inputs.definition import Definition
-
-
-class RunArgvInput(ArgvInput):
-    def __init__(
-        self,
-        argv: list[str] | None = None,
-        definition: Definition | None = None,
-    ) -> None:
-        super().__init__(argv, definition=definition)
-
-        self._parameter_options: list[str] = []
-
-    @property
-    def first_argument(self) -> str | None:
-        return "run"
-
-    def add_parameter_option(self, name: str) -> None:
-        self._parameter_options.append(name)
-
-    def has_parameter_option(
-        self, values: str | list[str], only_params: bool = False
-    ) -> bool:
-        if not isinstance(values, list):
-            values = [values]
-
-        for token in self._tokens:
-            if only_params and token == "--":
-                return False
-
-            for value in values:
-                if value not in self._parameter_options:
-                    continue
-
-                # Options with values:
-                # For long options, test for '--option=' at beginning
-                # For short options, test for '-o' at beginning
-                leading = value + "=" if value.startswith("--") else value
-
-                if token == value or leading != "" and token.startswith(leading):
-                    return True
-
-        return False
-
-    def _parse(self) -> None:
-        parse_options = True
-        self._parsed = self._tokens[:]
-
-        try:
-            token = self._parsed.pop(0)
-        except IndexError:
-            token = None
-
-        while token is not None:
-            if parse_options and token == "":
-                self._parse_argument(token)
-            elif parse_options and token == "--":
-                parse_options = False
-            elif parse_options and token.find("--") == 0:
-                if token in self._parameter_options:
-                    self._parse_long_option(token)
-                else:
-                    self._parse_argument(token)
-            elif parse_options and token[0] == "-" and token != "-":
-                if token in self._parameter_options:
-                    self._parse_short_option(token)
-                else:
-                    self._parse_argument(token)
-            else:
-                self._parse_argument(token)
-
-            try:
-                token = self._parsed.pop(0)
-            except IndexError:
-                token = None

tests/console/commands/test_run.py
@@ -51,6 +51,74 @@ def test_run_passes_all_args(app_tester: ApplicationTester, env: MockEnv) -> Non
     assert env.executed == [["python", "-V"]]
 
 
+def test_run_is_not_eager(app_tester: ApplicationTester, env: MockEnv) -> None:
+    app_tester.execute("--no-ansi -C run -install", decorated=True)
+    assert (
+        app_tester.io.fetch_error().strip()
+        == "Specified path 'run' is not a valid directory."
+    )
+    assert env.executed == []
+
+
+def test_run_passes_args_after_run_before_command(
+    app_tester: ApplicationTester, env: MockEnv
+) -> None:
+    app_tester.execute("run -P. python -V", decorated=True)
+    assert env.executed == [["python", "-V"]]
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        "-vP run run",
+        "run -vP run",
+        "-vPrun run",
+        "run -vPrun ",
+        "-v --project=run run",
+        "-v run --project=run",
+        "-v --directory=run run",
+        "run -v --directory=run",
+    ],
+)
+def test_run_passes_args_after_run_before_command_name_conflict(
+    args: str,
+    app_tester: ApplicationTester,
+    env: MockEnv,
+    project_factory: ProjectFactory,
+) -> None:
+    poetry = project_factory("run")
+    path = poetry.file.path.parent
+    path.rename(path.parent / "run")
+
+    app_tester.execute(f"{args} python -V", decorated=True)
+    assert app_tester.io.fetch_error() == ""
+    assert env.executed == [["python", "-V"]]
+
+
+def test_run_keeps_options_passed_before_command_args_combined_short_opts(
+    app_tester: ApplicationTester, env: MockEnv
+) -> None:
+    app_tester.execute("run -VP. --no-ansi python", decorated=True)
+
+    assert not app_tester.io.is_decorated()
+    assert app_tester.io.fetch_output() == app_tester.io.remove_format(
+        app_tester.application.long_version + "\n"
+    )
+    assert env.executed == []
+
+
+def test_run_keeps_options_passed_before_command_args(
+    app_tester: ApplicationTester, env: MockEnv
+) -> None:
+    app_tester.execute("run -V --no-ansi python", decorated=True)
+
+    assert not app_tester.io.is_decorated()
+    assert app_tester.io.fetch_output() == app_tester.io.remove_format(
+        app_tester.application.long_version + "\n"
+    )
+    assert env.executed == []
+
+
 def test_run_keeps_options_passed_before_command(
     app_tester: ApplicationTester, env: MockEnv
 ) -> None:

tests/console/test_application.py
@@ -5,6 +5,7 @@ import shutil
 
 from typing import TYPE_CHECKING
 from typing import ClassVar
+from typing import cast
 
 import pytest
 
@@ -24,8 +25,10 @@ from tests.helpers import mock_metadata_entry_points
 if TYPE_CHECKING:
     from pathlib import Path
 
+    from cleo.io.inputs.argv_input import ArgvInput
     from pytest_mock import MockerFixture
 
+    from tests.helpers import PoetryTestApplication
     from tests.types import FixtureDirGetter
     from tests.types import SetProjectContext
 
@@ -185,3 +188,60 @@ def test_application_verify_cache_flag_at_install(
             (name, args, kwargs) = call
             assert "disable_cache" in kwargs
             assert disable_cache is kwargs["disable_cache"]
+
+
+@pytest.mark.parametrize(
+    ("tokens", "result"),
+    [
+        (
+            ["-C", "/path/working/dir", "env", "list"],
+            ["--directory", "/path/working/dir", "env", "list"],
+        ),
+        (
+            ["-P", "/path/project/dir", "env", "list"],
+            ["--project", "/path/project/dir", "env", "list"],
+        ),
+        (
+            ["-P/path/project/dir", "env", "list"],
+            ["--project", "/path/project/dir", "env", "list"],
+        ),
+        (
+            ["-P/path/project/dir", "env", "list"],
+            ["--project", "/path/project/dir", "env", "list"],
+        ),
+        (
+            ["-v", "run", "-P/path/project/dir", "echo", "--help"],
+            [
+                "--verbose",
+                "--project",
+                "/path/project/dir",
+                "run",
+                "--",
+                "echo",
+                "--help",
+            ],
+        ),
+        (
+            ["--no-ansi", "run", "-V", "python", "-V"],
+            ["--version", "--no-ansi", "run", "--", "python", "-V"],
+        ),
+        (
+            ["--no-ansi", "run", "-V", "--", "python", "-V"],
+            ["--version", "--no-ansi", "run", "--", "python", "-V"],
+        ),
+    ],
+)
+def test_application_input_configuration_and_sorting(
+    tokens: list[str], result: list[str], app: PoetryTestApplication
+) -> None:
+    app.create_io()
+    assert app._io is not None
+
+    io_input = cast("ArgvInput", app._io.input)
+    io_input._tokens = tokens
+
+    app._configure_io(app._io)
+    app._sort_global_options(app._io)
+
+    io_input = cast("ArgvInput", app._io.input)
+    assert io_input._tokens == result