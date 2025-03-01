72d5532f4dea1eddb869842d93d31e661da9547d
Martin Votruba <martin_votruba@rapid7.com>
2025-03-01 10:48:59 +0100

    adding prints

--- tests/console/commands/test_check.py
+++ tests/console/commands/test_check.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from typing import TYPE_CHECKING
 
 import pytest
@@ -324,6 +325,12 @@ def test_check_project_with_src_folder(
     poetry_with_src_folder: Poetry,
 ) -> None:
     tester = command_tester_factory("check", poetry=poetry_with_src_folder)
+
+    poetry_file = poetry_with_src_folder.file.path
+    project_root = poetry_file.parent
+    project_dir = project_root / "project_with_src_folder"
+    print(project_dir)
+    os.makedirs(project_dir, exist_ok=True)
     tester.execute("")
 
     expected = """\
