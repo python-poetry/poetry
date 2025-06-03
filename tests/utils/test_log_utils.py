c09bfdc790fcfdd8637f15391c617345375c0975
Robertino Bogart <robertino.bogart@churchdwight.com>
2025-06-03 18:38:01 -0400

    test: replace dummy classes with real Package and MockEnv instances

--- tests/utils/test_log_utils.py
+++ tests/utils/test_log_utils.py
@@ -1,35 +1,14 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-from typing import cast
+from poetry.core.packages.package import Package
 
+from poetry.utils.env.mock_env import MockEnv
 from poetry.utils.log_utils import format_build_wheel_log
 
 
-if TYPE_CHECKING:
-    from poetry.core.packages.package import Package
-
-    from poetry.utils.env import Env
-
-
-class DummyEnv:
-    @property
-    def marker_env(self) -> dict[str, object]:
-        return {
-            "version_info": (3, 13, 1),
-            "sys_platform": "win32",
-            "platform_machine": "AMD64",
-        }
-
-
-class DummyPackage:
-    pretty_name = "demo"
-    full_pretty_version = "1.2.3"
-
-
 def test_format_build_wheel_log() -> None:
-    env = cast("Env", DummyEnv())
-    package = cast("Package", DummyPackage())
+    env = MockEnv(version_info=(3, 13, 1), platform="win32", platform_machine="AMD64")
+    package = Package(name="demo", version="1.2.3")
     result = format_build_wheel_log(package, env)
     expected = (
         " <info>Building a wheel file for demo "
