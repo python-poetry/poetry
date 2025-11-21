92190c5fc85433151e21f6be836dc3e2b3a41ce4
Randy Döring <30527984+radoering@users.noreply.github.com>
2025-11-21 06:33:39 +0100

    publish: use the artifact metadata instead of the project metadata (#10624)
    Especially, since we do support other build backends than poetry-core this is more correct.

--- tests/publishing/test_uploader.py
+++ tests/publishing/test_uploader.py
@@ -155,3 +155,78 @@ def test_uploader_properly_handles_file_not_existing(
         uploader.upload("https://foo.com")
 
     assert f"Archive ({uploader.files[0]}) does not exist" == str(e.value)
+
+
+def test_uploader_post_data_wheel(fixture_dir: FixtureDirGetter) -> None:
+    file = (
+        fixture_dir("simple_project")
+        / "dist"
+        / "simple_project-1.2.3-py2.py3-none-any.whl"
+    )
+    assert Uploader.post_data(file) == {
+        "md5_digest": "fb4a5266406b9cf34ceaa88d1c8b7a01",
+        "sha256_digest": "fc365a242d4de8b8661babc088f44b3df25e9e0017ef5dd7140dfe50f9323e16",
+        "blake2_256_digest": "2e006d1fbfef0ed38fbded1ec1614dc4fd66f81061fe290528e2744dbc25ce31",
+        "filetype": "bdist_wheel",
+        "pyversion": "py2.py3",
+        "metadata_version": "2.1",
+        "name": "simple-project",
+        "version": "1.2.3",
+        "summary": "Some description.",
+        "author": "Sébastien Eustace",
+        "author_email": "sebastien@eustace.io",
+        "license": "MIT",
+        "classifiers": [
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Topic :: Software Development :: Build Tools",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+        "requires_python": ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
+        "description": "My Package\n==========\n\n",
+        "description_content_type": "text/x-rst",
+        "keywords": "packaging, dependency, poetry",
+        "home_page": "https://poetry.eustace.io",
+        "project_urls": [
+            "Documentation, https://poetry.eustace.io/docs",
+            "Repository, https://github.com/sdispater/poetry",
+        ],
+    }
+
+
+def test_uploader_post_data_sdist(fixture_dir: FixtureDirGetter) -> None:
+    file = fixture_dir("simple_project") / "dist" / "simple_project-1.2.3.tar.gz"
+    assert Uploader.post_data(file) == {
+        "md5_digest": "e611cbb8f31258243d90f7681dfda68a",
+        "sha256_digest": "c4a72becabca29ec2a64bf8c820bbe204d2268f53e102501ea5605bc1c1675d1",
+        "blake2_256_digest": "d3df22f4944f6acd02105e7e2df61ef63c7b0f4337a12df549ebc2805a13c2be",
+        "filetype": "sdist",
+        "pyversion": "source",
+        "metadata_version": "2.1",
+        "name": "simple-project",
+        "version": "1.2.3",
+        "summary": "Some description.",
+        "author": "Sébastien Eustace",
+        "author_email": "sebastien@eustace.io",
+        "classifiers": [
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Topic :: Software Development :: Build Tools",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+        "requires_python": ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
+        "keywords": "packaging, dependency, poetry",
+        "home_page": "https://poetry.eustace.io",
+        "project_urls": [
+            "Documentation, https://poetry.eustace.io/docs",
+            "Repository, https://github.com/sdispater/poetry",
+        ],
+    }
