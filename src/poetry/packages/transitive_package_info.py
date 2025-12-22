e8a05b7aea2bbf4a06f4e4886dd5353ff5f13a1f
Randy Döring <30527984+radoering@users.noreply.github.com>
2025-12-22 09:50:41 +0100

    fix indeterministic markers (#10667)
    Currently, this is only relevant for poetry-plugin-export because in poetry itself, we only evaluate the marker.

--- src/poetry/packages/transitive_package_info.py
+++ src/poetry/packages/transitive_package_info.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from poetry.core.packages.dependency_group import MAIN_GROUP
 from poetry.core.version.markers import BaseMarker
 from poetry.core.version.markers import EmptyMarker
 
@@ -13,6 +14,10 @@ if TYPE_CHECKING:
     from packaging.utils import NormalizedName
 
 
+def group_sort_key(group: NormalizedName) -> tuple[bool, NormalizedName]:
+    return group != MAIN_GROUP, group
+
+
 @dataclass
 class TransitivePackageInfo:
     depth: int  # max depth in the dependency tree
@@ -21,7 +26,7 @@ class TransitivePackageInfo:
 
     def get_marker(self, groups: Iterable[NormalizedName]) -> BaseMarker:
         marker: BaseMarker = EmptyMarker()
-        for group in groups:
+        for group in sorted(groups, key=group_sort_key):
             if group_marker := self.markers.get(group):
                 marker = marker.union(group_marker)
         return marker
