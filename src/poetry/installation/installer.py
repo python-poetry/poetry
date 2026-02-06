bd19ef1fad1080ee9d3968fef6a2586378d8e800
Louis Shawn <louis.shawn@qq.com>
2026-02-06 09:14:44 +0800

    refactor: avoid magic string for self project

--- src/poetry/installation/installer.py
+++ src/poetry/installation/installer.py
@@ -33,6 +33,7 @@ if TYPE_CHECKING:
 
 
 class Installer:
+    _POETRY_SYSTEM_PROJECT_NAME = "poetry-instance"
     def __init__(
         self,
         io: IO,
@@ -100,7 +101,7 @@ class Installer:
     def _lock_fix_command(self) -> str:
         # `poetry self` commands operate on Poetry's own system project. When the lock
         # file is outdated, users should run `poetry self lock` rather than `poetry lock`.
-        if self._package.name == "poetry-instance":
+        if self._package.name == self._POETRY_SYSTEM_PROJECT_NAME:
             return "poetry self lock"
 
         return "poetry lock"
