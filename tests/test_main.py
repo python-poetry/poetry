35832ccab34fc39dfb7767a18782b53085dc38cd
Randy Döring <30527984+radoering@users.noreply.github.com>
2026-05-20 18:09:16 +0200

    use mocker fixture

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,22 +2,20 @@ from __future__ import annotations
 
 import runpy
 
+from typing import TYPE_CHECKING
+
 import pytest
 
 
-def test_module_entrypoint_exits_with_application_status(
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    calls: list[None] = []
+if TYPE_CHECKING:
+    from pytest_mock import MockerFixture
 
-    def main() -> int:
-        calls.append(None)
-        return 17
 
-    monkeypatch.setattr("poetry.console.application.main", main)
+def test_module_entrypoint_exits_with_application_status(mocker: MockerFixture) -> None:
+    main_mock = mocker.patch("poetry.console.application.main", side_effect=lambda: 17)
 
     with pytest.raises(SystemExit) as e:
-        runpy.run_module("poetry", run_name="__main__", alter_sys=True)
+        runpy.run_module("poetry", run_name="__main__")
 
     assert e.value.code == 17
-    assert calls == [None]
+    assert main_mock.call_count == 1
