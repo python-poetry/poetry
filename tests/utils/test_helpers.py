79693457ae8375dbe0d19bbe95a93be87ede6ef9
Bartosz Sokorski <bartosz.sokorski@stxnext.pl>
2026-05-28 14:45:41 +0200

    chore(tests): remove poetry.core-based test

--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -13,7 +13,6 @@ from typing import Any
 import pytest
 import responses
 
-from poetry.core.utils.helpers import parse_requires
 from requests.exceptions import ChunkedEncodingError
 
 from poetry.utils._compat import WINDOWS
@@ -32,62 +31,6 @@ if TYPE_CHECKING:
     from tests.conftest import Config
     from tests.types import FixtureDirGetter
     from tests.types import HttpResponse
-
-
-def test_parse_requires() -> None:
-    requires = """\
-jsonschema>=2.6.0.0,<3.0.0.0
-lockfile>=0.12.0.0,<0.13.0.0
-pip-tools>=1.11.0.0,<2.0.0.0
-pkginfo>=1.4.0.0,<2.0.0.0
-pyrsistent>=0.14.2.0,<0.15.0.0
-toml>=0.9.0.0,<0.10.0.0
-cleo>=0.6.0.0,<0.7.0.0
-cachy>=0.1.1.0,<0.2.0.0
-cachecontrol>=0.12.4.0,<0.13.0.0
-requests>=2.18.0.0,<3.0.0.0
-msgpack-python>=0.5.0.0,<0.6.0.0
-pyparsing>=2.2.0.0,<3.0.0.0
-requests-toolbelt>=0.8.0.0,<0.9.0.0
-
-[:(python_version >= "2.7.0.0" and python_version < "2.8.0.0")\
- or (python_version >= "3.4.0.0" and python_version < "3.5.0.0")]
-typing>=3.6.0.0,<4.0.0.0
-
-[:python_version >= "2.7.0.0" and python_version < "2.8.0.0"]
-virtualenv>=15.2.0.0,<16.0.0.0
-pathlib2>=2.3.0.0,<3.0.0.0
-
-[:python_version >= "3.4.0.0" and python_version < "3.6.0.0"]
-zipfile36>=0.1.0.0,<0.2.0.0
-
-[dev]
-isort@ git+git://github.com/timothycrosley/isort.git@e63ae06ec7d70b06df9e528357650281a3d3ec22#egg=isort
-"""
-    result = parse_requires(requires)
-    # fmt: off
-    expected = [
-        "jsonschema>=2.6.0.0,<3.0.0.0",
-        "lockfile>=0.12.0.0,<0.13.0.0",
-        "pip-tools>=1.11.0.0,<2.0.0.0",
-        "pkginfo>=1.4.0.0,<2.0.0.0",
-        "pyrsistent>=0.14.2.0,<0.15.0.0",
-        "toml>=0.9.0.0,<0.10.0.0",
-        "cleo>=0.6.0.0,<0.7.0.0",
-        "cachy>=0.1.1.0,<0.2.0.0",
-        "cachecontrol>=0.12.4.0,<0.13.0.0",
-        "requests>=2.18.0.0,<3.0.0.0",
-        "msgpack-python>=0.5.0.0,<0.6.0.0",
-        "pyparsing>=2.2.0.0,<3.0.0.0",
-        "requests-toolbelt>=0.8.0.0,<0.9.0.0",
-        'typing>=3.6.0.0,<4.0.0.0 ; (python_version >= "2.7.0.0" and python_version < "2.8.0.0") or (python_version >= "3.4.0.0" and python_version < "3.5.0.0")',
-        'virtualenv>=15.2.0.0,<16.0.0.0 ; python_version >= "2.7.0.0" and python_version < "2.8.0.0"',
-        'pathlib2>=2.3.0.0,<3.0.0.0 ; python_version >= "2.7.0.0" and python_version < "2.8.0.0"',
-        'zipfile36>=0.1.0.0,<0.2.0.0 ; python_version >= "3.4.0.0" and python_version < "3.6.0.0"',
-        'isort@ git+git://github.com/timothycrosley/isort.git@e63ae06ec7d70b06df9e528357650281a3d3ec22#egg=isort ; extra == "dev"',
-    ]
-    # fmt: on
-    assert result == expected
 
 
 def test_default_hash(fixture_dir: FixtureDirGetter) -> None:
